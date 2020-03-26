f305e8c44d9b635681649f27d31066f66e1a4ad7
Alexander Rix <alexanderjrix@gmail.com>
2020-03-26 15:05:33 -0400

    rework plot2 code and data stream

--- app/server.R
+++ app/server.R
@@ -8,13 +8,13 @@ library(glue)
 shinyServer(function(input, output)
 {
     plot1_input <- function() {
-        start.date <- as.Date("2020-01-30")
+        start.date <- as.Date("2020-03-01")
 
         jhu.path <- "https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series"
         jhu.files <- list(
-        Case      = paste0(jhu.path, "/time_series_covid19_confirmed_global.csv"),
-        Recovered = paste0(jhu.path, "/time_series_covid19_recovered_global.csv"),
-        Death    = paste0(jhu.path, "/time_series_covid19_deaths_global.csv")
+            Case      = paste0(jhu.path, "/time_series_covid19_confirmed_global.csv"),
+            Recovered = paste0(jhu.path, "/time_series_covid19_recovered_global.csv"),
+            Death    = paste0(jhu.path, "/time_series_covid19_deaths_global.csv")
         )
         data <- reduce(imap(jhu.files,
             function(file, var)
@@ -32,25 +32,22 @@ shinyServer(function(input, output)
         gather(Case, Recovered, Death, key = Type, value = Count) %>%
         mutate(Date = as.factor(format(Date, format = "%b.%d")))
 
-        # dataf$Date <- format(as.Date(dataf$Date, format="%m-%d-%y"), format="%b.%d")
-        # dataf$Date <- factor(dataf$Date, levels=dataf$Date)
-
-        myylab <- "Number of new cases/recovered/deaths"
-
         p <- ggplot(data, aes(Date, Count))
         p <- p + geom_bar(stat = "identity", aes(fill = Type), position = "stack") +
-            xlab("") + ylab(myylab) +
+        xlab("") + ylab("Number of new cases/recovered/deaths") +
             labs(subtitle = "Data source: Johns Hopkins University CSSE",
                  caption = "© COV-IND-19 Study Group") +
             theme_bw() +
             theme(axis.text.x = element_text(angle = 45, vjust = 0.5, size=15),
                   axis.text.y = element_text(size = 15),
-                  legend.position="bottom",
-                  legend.title = element_blank(), #legend.box = "horizontal",
-                  legend.text=element_text(size = 17), plot.title =
+                  legend.position = "bottom", legend.title = element_blank(),
+                  legend.text = element_text(size = 17), plot.title =
                   element_text(size = 18), axis.title.y = element_blank(),
-                  plot.caption = element_text(color = "blue", face = "bold", size = 10)) +
-            scale_fill_manual("", values = c("Case" = "orange", "Recovered" = "dark green", "Death" = "red"))
+                  plot.caption = element_text(color = "blue", face = "bold",
+                  size = 10)
+            ) +
+            scale_fill_manual("", values = c("Case" = "orange", "Recovered" =
+                              "dark green", "Death" = "red"))
         print(p)
     }
 
@@ -68,52 +65,48 @@ shinyServer(function(input, output)
     )
 
     plot2_input <- function() {
-        options(stringsAsFactors=F)
-        Data=read.csv("https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_19-covid-Confirmed.csv&filename=time_series_2019-ncov-Confirmed.csv")
-        Data=Data[,-c(1,3,4)]
 
-        Data.Country=aggregate(.~Country.Region,data=Data,sum,na.rm=TRUE)
+        jhu.path <- "https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series"
 
-        countries <- c("France","Germany","India","Iran","Italy","Japan","Korea, South","Singapore","US","China")
-        # countries <- c("India")
+        countries <- c("France", "Germany", "India", "Iran", "Italy",
+                       "Korea, South", "US", "China")
 
-        Data.Subset=Data.Country[Data.Country$Country.Region %in% countries,]
-        HundredPlus=unlist(lapply(1:nrow(Data.Subset),function(x){return(min(which(Data.Subset[x,-1]>100)))}))
-        MaxCols=max(HundredPlus)+((ncol(Data.Subset)-1)-min(HundredPlus))
+        file <- paste0(jhu.path, "/time_series_covid19_confirmed_global.csv")
+        data <- vroom(file) %>%
+        select(Country = matches("Country"), matches("[0-9].*")) %>%
+        filter(Country %in% countries) %>%
+        group_by(Country) %>%
 
-        Data.New=matrix(0,nrow(Data.Subset),MaxCols)
-        for(i in 1:nrow(Data.New))
-        {
-            Range=(1:(ncol(Data.Subset)-1))+(max(HundredPlus)-HundredPlus[i])
-            Replace=c(rep(NA,min(Range)-1),as.vector(Data.Subset[i,-1]),rep(NA,MaxCols-max(Range)))
-            Data.New[i,]=unlist(Replace)
-        }
-        Data.New=data.frame(Country=as.character(Data.Subset$Country.Region),Data.New)
-        colnames(Data.New)[-1]=(1:MaxCols)-max(HundredPlus)
-        Data.New[8,1]="South Korea"
+        # Since we don't care about counts in each state we collapse into a
+        # single count per country of interest.
+        summarise_all(sum, na.rm = T) %>%
+        gather(matches("[0-9].+"), key = Date, value = Cases) %>%
+        mutate(Date = as.Date(Date, format = "%m/%d/%y")) %>%
+        group_by(Country) %>% filter(Cases >= 100) %>%
+        arrange(Date) %>%
+        mutate(Day = seq(n()))
 
-        Data.Plot=melt(Data.New,id="Country")
-        colnames(Data.Plot)[-1]=c("Days","Cases")
+        Day.max <- nrow(data %>% filter(Country == "India"))
+        data <- filter(data, Day <= Day.max) %>%
+        mutate(Day = as.factor(Day)) %>%
+        ungroup()
 
-
-        p <- ggplot(Data.Plot[Data.Plot$Days%in%(-1:7),],
-                    mapping=aes(x=Days,y=Cases,colour=Country,group=Country))+
-            geom_point(size=3.6,na.rm=TRUE)+
-            geom_path(size=1.8,na.rm=TRUE)+
-            xlab("Days since infected cases reach 100")+
-            ylab("Number of infected cases")+
-            ggtitle("Cumulative number of COVID-19 infections", "Updated on March 22")+
-            theme_bw()+labs(caption="\uA9 COV-IND-19 Study Group")+
-            theme(axis.text.x=element_text(#angle=90,
-                vjust=0.5,size=15),
+        p <- ggplot(data, aes(Day, Cases, col = Country, group = Country)) +
+            geom_point(size = 3.6, na.rm = TRUE) +
+            geom_path(size = 1.8, na.rm = TRUE) +
+            xlab("Days since infected cases reached 100")+
+            ylab("Number of infected cases") +
+            theme_bw() + labs(caption = "\uA9 COV-IND-19 Study Group")+
+            theme(axis.text.x = element_text(
+                vjust = 0.5, size = 15),
                 legend.position = "bottom",
-                axis.text.y=element_text(size=15),
-                plot.title=element_text(size=18),
-                plot.caption=element_text(color="blue", face="bold"),
-                #legend.position=c(0.1,0.6),
-                legend.title=element_blank(),
-                legend.box="horizontal",
-                legend.text=element_text(size=17))
+                axis.text.y = element_text(size = 15),
+                plot.title = element_text(size = 18),
+                plot.caption = element_text(color = "blue", face = "bold"),
+                #legend.position = c(0.1,0.6),
+                legend.title = element_blank(),
+                legend.box = "horizontal",
+                legend.text = element_text(size = 17))
         print(p)
     }
 
