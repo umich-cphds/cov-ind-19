5876b92376dad5e2466f745456d1a28f6ffb8990
Alexander Rix <alexanderjrix@gmail.com>
2020-03-28 19:20:48 -0400

    fix typo

--- app/ui.R
+++ app/ui.R
@@ -32,7 +32,8 @@ shinyUI(fluidPage(
              br(),
              p("Read the article: ", a("Medium article", target = "_blank", href = "https://medium.com/@covind_19/predictions-and-role-of-interventions-for-covid-19-outbreak-in-india-52903e2544e6")),
              p("Read the report: ", a("COV-IND-19 Report", target = "_blank", href = "https://bit.ly/COV-IND-19_Report"), "(this is a direct download link, check your downloads folder)"),
-             p("Date source: ", a("JHU CSSE COVID-19 GitHub", target = "_blank", href = "https://github.com/CSSEGISandData/COVID-19")),
+             p("Data source: ", a("JHU CSSE COVID-19 GitHub", target = "_blank", href = "https://github.com/CSSEGISandData/COVID-19")),
+             p("Data source: ", a("COVID-19 in India Kaggle", target = "_blank", href = "https://www.kaggle.com/sudalairajkumar/covid19-in-india")),
              p("R modeling package: ", a("eSIR R package", target = "_blank", href = "https://github.com/lilywang1988/eSIR")),
              p("Source code: ", a("COV-IND-19 GitHub", target = "_blank", href = "https://github.com/umich-cphds/cov-ind-19")),
              p("Please direct any questions or inquiries to ", a("Bhramar Mukherjee", target="_blank", href="mailto:bhramar@umich.edu"))
@@ -43,7 +44,7 @@ shinyUI(fluidPage(
     # Show a plot of the generated distribution
     mainPanel(
         h2("Daily number of COVID-19 new cases, fatalities and recovered cases in India from March 1 to current date"),
-        p("This figure provides the number of COVID-19 new cases (yellow), fatalities (red), and recovered cases (green) in India. 
+        p("This figure provides the number of COVID-19 new cases (yellow), fatalities (red), and recovered cases (green) in India.
           You can hover your cursor over the bar to see the exact numerical counts."),
         plotlyOutput("plot1", height = "600px"),
         downloadButton("download_plot1", label = "Download Figure 1"),
@@ -62,9 +63,9 @@ shinyUI(fluidPage(
         p("The map displays the case counts by state/union territory in India over the last few days.",
           "The darker areas of the map indicate a greater number of cases."),
         fluidRow(
-            column(width = 3), 
+            column(width = 3),
             column(width = 9, imageOutput("map", height = "650px")),
-        ), 
+        ),
         hr(),
         h2(HTML(paste0("Predicted cases in India until June 15 (assuming R", tags$sub("0"), " = 2)"))),
         p("This figure plots the observed number of cases up until today and then forecasts the number of cases until June 15.",
