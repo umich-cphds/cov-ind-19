ec8e120681669aac53717b5b0014e815ac38ec0f
Kleinsasser <mkleinsa@UMICH.EDU>
2020-03-29 10:21:17 -0400

    interactive fig 5, and removed title in interactive mode

--- app/server.R
+++ app/server.R
@@ -299,11 +299,13 @@ shinyServer(function(input, output)
         }
     )
 
-    output$plot5a_full <- renderPlot({
-        readRDS(url(paste0(github.path, latest, "/1wk/Figure5.Rds"))) + 
+    output$plot5a_full <- renderPlotly({
+        gplot = readRDS(url(paste0(github.path, latest, "/1wk/Figure5.Rds"))) + 
             theme(plot.title = element_blank(),
                   plot.caption = element_blank(),
                   plot.subtitle = element_blank())
+        gplot$labels$title = ""
+        plotly::ggplotly(gplot, layerData = 1)
     })
     
     output$download_plot5a <- downloadHandler(
@@ -315,11 +317,13 @@ shinyServer(function(input, output)
         }
     )
     
-    output$plot5b_full <- renderPlot({
-        readRDS(url(paste0(github.path, latest, "/2wk/Figure5.Rds"))) + 
+    output$plot5b_full <- renderPlotly({
+        gplot = readRDS(url(paste0(github.path, latest, "/2wk/Figure5.Rds"))) + 
             theme(plot.title = element_blank(),
                   plot.caption = element_blank(),
                   plot.subtitle = element_blank())
+        gplot$labels$title = ""
+        plotly::ggplotly(gplot, layerData = 1)
     })
     
     output$download_plot5b <- downloadHandler(
