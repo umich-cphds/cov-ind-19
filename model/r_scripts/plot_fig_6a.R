35b635cab1e4ef46e44fcbebebc55430808c3592
Alexander Rix <alexanderjrix@gmail.com>
2020-03-31 12:24:20 -0400

    add the rest of the server side processing scripts

---
+++ model/r_scripts/plot_fig_6a.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+library(vroom)
+library(plotly)
+
+start.date <- as.Date("2020-03-01")
+latest <- Sys.Date()
+
+data <- vroom(paste0("~/cov-ind-19-data/", latest, "/2wk/figure_5_data.csv")) %>%
+mutate(text = paste0(format(Dates, "%b %d"),": ",
+                     round(value),
+                     " projected total cases")
+)
+
+
+cap <- paste0("© COV-IND-19 Study Group. Last updated: ",
+              format(latest, format = "%b %d"), sep = ' ')
+
+axis.title.font <- list(size = 16)
+tickfont        <- list(size = 16)
+
+xaxis <- list(title = "Date",
+              titlefont = axis.title.font, showticklabels = TRUE,
+              tickangle = -30, zeroline = F)
+
+yaxis <- list(title = "Total number of infected cases per 100,000",
+              titlefont = axis.title.font, zeroline = T)
+
+colors <- c("#173F5F", "#0472CF", "#3CAEA3", "#f2c82e")
+p <- plot_ly(data, x = ~Dates, y = ~ value * 1e5 / 1.34e9, text = ~text,
+        color = ~ color, colors = colors, type = "scatter", mode = "line",
+        hoverinfo = "text", line = list(width = 4)
+) %>%
+layout(xaxis = xaxis, yaxis = yaxis,
+       title = list(text = cap, xanchor = "left", x = 0)
+)
+
+saveRDS(p, paste0("~/cov-ind-19-data/", latest, "/plot6a.RDS"))
