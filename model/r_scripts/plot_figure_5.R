207de2fc8fce463ed1a81ba139d0ea9cb14c1fff
Max Salvatore <38917700+maxsal@users.noreply.github.com>
2020-04-01 10:44:07 -0400

    Update plot_figure_5.R

--- model/r_scripts/plot_figure_5.R
+++ model/r_scripts/plot_figure_5.R
@@ -24,7 +24,8 @@ forecast_length <- 200
 forecast_dt     <- seq(from = as.Date(dates_1[length(dates_1)] + 1, format = "%m-%d-%y"), to = as.Date(dates_1[length(dates_1)] + forecast_length, format = "%m-%d-%y"), "days")
 adj_len         <- 2
 adj             <- T
-plot_end_date   <- "2020-09-15"
+plot_start_date <- "2020-04-30"
+plot_end_date   <- "2020-08-30"
 
 # function ----------
 fig_5_data <- function(x) {
@@ -48,29 +49,24 @@ fig_5_data <- function(x) {
   return(list(india_confirm, india_confirm_up))
 }
 
-# processinng ----------
-mod_2 <- fig_5_data("India_2_plot_data.RData")
-
+# processing ----------
+mod_2    <- fig_5_data("India_2_plot_data.RData")
 mod_2_up <- mod_2[[2]]
 mod_2    <- mod_2[[1]]
 
-mod_3 <- fig_5_data("India_3_plot_data.RData")
-
+mod_3    <- fig_5_data("India_3_plot_data.RData")
 mod_3_up <- mod_3[[2]]
 mod_3    <- mod_3[[1]]
 
-mod_4 <- fig_5_data("India_4_plot_data.RData")
-
+mod_4    <- fig_5_data("India_4_plot_data.RData")
 mod_4_up <- mod_4[[2]]
 mod_4    <- mod_4[[1]]
 
-mod_5 <- fig_5_data("India_5_plot_data.RData")
-
+mod_5    <- fig_5_data("India_5_plot_data.RData")
 mod_5_up <- mod_5[[2]]
 mod_5    <- mod_5[[1]]
 
-mod_6 <- fig_5_data("India_6_plot_data.RData")
-
+mod_6    <- fig_5_data("India_6_plot_data.RData")
 mod_6_up <- mod_6[[2]]
 mod_6    <- mod_6[[1]]
 
@@ -80,27 +76,27 @@ observed_plot <- tibble(
   value    = dataf
   )
 
-# mod_2               <- mod_2 - dplyr::lag(mod_2)
-# mod_3               <- mod_3 - dplyr::lag(mod_3)
-# mod_4               <- mod_4 - dplyr::lag(mod_4)
-# mod_5               <- mod_5 - dplyr::lag(mod_5)
-# mod_6               <- mod_6 - dplyr::lag(mod_6)
-# observed_plot$value <- observed_plot$value - lag(observed_plot$value)
-
 forecasts_plot <- tibble(
   Dates    = forecast_dt,
   mod_2    = mod_2,
-  mod_2_up = mod_2_up,
   mod_3    = mod_3,
-  mod_3_up = mod_3_up,
   mod_4    = mod_4,
-  mod_4_up = mod_4_up,
   mod_5    = mod_5,
-  mod_5_up = mod_5_up,
   mod_6    = mod_6,
-  mod_6_up = mod_6_up,
   ) %>%
-  gather(variable, value, -Dates) 
+  gather(variable, value, -Dates)
+
+forecasts_plot_ci <- tibble(
+  Dates    = forecast_dt,
+  mod_2 = mod_2_up,
+  mod_3 = mod_3_up,
+  mod_4 = mod_4_up,
+  mod_5 = mod_5_up,
+  mod_6 = mod_6_up,
+  ) %>%
+  gather(variable, upper_ci, -Dates)
+
+forecasts_plot <- left_join(forecasts_plot, forecasts_plot_ci, by = c("Dates", "variable"))
 
 connect_plot <- tibble(
   Dates    = rep(as.Date("03-23-2020", format = "%m-%d-%y"), 5),
@@ -115,18 +111,13 @@ complete_plot <- bind_rows(observed_plot,
   arrange(Dates) %>%
   mutate(
     color = as.factor(case_when(
-      variable == "True"     ~ "Observed",
-      variable == "mod_2"    ~ "Soc. Dist. + Travel Ban",
-      variable == "mod_2_up" ~ "Soc. Dist. + Travel Ban CI",
-      variable == "mod_3"    ~ "No Intervention",
-      variable == "mod_3_up" ~ "No Intervention CI",
-      variable == "mod_4"    ~ "Moderate return",
-      variable == "mod_4_up" ~ "Moderate return CI",
-      variable == "mod_5"    ~ "Normal (pre-intervention)",
-      variable == "mod_5_up" ~ "Normal (pre-intervention) CI",
-      variable == "mod_6"    ~ "Cautious return",
-      variable == "mod_6_up" ~ "Cautious return CI",
-      variable == "Limit"    ~ "Limit"
+      variable == "True" ~ "Observed",
+      variable == "mod_2" ~ "Soc. Dist. + Travel Ban",
+      variable == "mod_3" ~ "No Intervention",
+      variable == "mod_4" ~ "Moderate activity",
+      variable == "mod_5" ~ "Normal (pre-intervention)",
+      variable == "mod_6" ~ "Hesitant",
+      variable == "Limit" ~ "Limit"
     )),
     type = as.factor(if_else(variable == "Limit", "dashed", "solid"))
   )
@@ -134,12 +125,11 @@ complete_plot <- bind_rows(observed_plot,
 write_csv(complete_plot, path = "./figure_5_data.csv")
 
 complete_plot <- complete_plot %>%
-  filter(Dates <= as.Date(plot_end_date, format = "%Y-%m-%d") & Dates >= as.Date("2020-04-15", format = "%Y-%m-%d")) %>%
-  filter(!color %in% c("No Intervention", "No Intervention CI", "Soc. Dist. + Travel Ban CI", "Moderate return CI",
-                       "Normal (pre-intervention) CI", "Cautious return CI"))
+  filter(Dates <= as.Date(plot_end_date, format = "%Y-%m-%d") & Dates >= as.Date(plot_start_date, format = "%Y-%m-%d")) %>%
+  filter(color != "No Intervention")
 
-complete_plot_solid <- complete_plot %>% filter(type == 'solid')
-complete_plot_dash  <- complete_plot %>% filter(type == 'dashed')
+complete_plot_solid <- complete_plot %>% filter(type == "solid")
+complete_plot_dash  <- complete_plot %>% filter(type == "dashed")
 
 # plot ----------
 color_values <- c("Soc. Dist. + Travel Ban"   = "#f2c82e",
@@ -154,23 +144,23 @@ my_title    <- paste0("Predicted number of COVID-19 infections")
 my_subtitle <- paste0("as of ", format(latest_date, "%d %B, %Y"))
 
 p1 <- ggplot(data = complete_plot_solid, mapping = aes(x = Dates, y = value * 100000 / !!N, group = variable, color = color)) +
-  geom_smooth(se = FALSE, span = 0.2) + 
+  geom_smooth(se = FALSE, span = 0.2) +
   scale_color_manual(values = color_values) +
   scale_x_date(labels = date_format("%b %d")) +
-  ylim(0, 250) +
+  # ylim(0, 250) +
   labs(title    = my_title,
        subtitle = my_subtitle,
        y        = "Number of infected cases per 100,000 people in India",
        x        = "Date",
        color    = "Scenario",
        caption  = "\uA9 COV-IND-19 Study Group") +
-  theme_bw() +
-  theme(axis.text.x  = element_text(angle = 45, vjust = 0.5, size = 15),
-        axis.text.y  = element_text(size = 15),
-        plot.title   = element_text(size = 22),
-        plot.caption = element_text(color = "blue",face = "bold"))
+  theme_minimal() +
+  theme(axis.text.x      = element_text(angle = 45, vjust = 0.5, size = 15),
+        axis.text.y      = element_text(size = 15),
+        plot.title       = element_text(size = 22),
+        legend.position  = "bottom",
+        plot.caption     = element_text(color = "blue",face = "bold"))
 
 p1
-
 
 write_rds(p1, "./Figure5.Rds")
