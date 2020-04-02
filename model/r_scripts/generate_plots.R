921afbdb1e4161d25504995262d50f65e980ffc7
Alexander Rix <alexanderjrix@gmail.com>
2020-04-02 15:03:04 -0400

    package plots

--- model/r_scripts/generate_plots.R
+++ model/r_scripts/generate_plots.R
@@ -25,3 +25,19 @@ plot_fig_5a(latest = latest)
 plot_fig_5b(latest = latest)
 plot_fig_6a(latest = latest)
 plot_fig_6b(latest = latest)
+
+
+p1 <- readRDS(paste0("~/cov-ind-19-data/", latest, "/plot1.RDS"))
+p2 <- readRDS(paste0("~/cov-ind-19-data/", latest, "/plot2.RDS"))
+p3 <- readRDS(paste0("~/cov-ind-19-data/", latest, "/plot3.RDS"))
+p4a <- readRDS(paste0("~/cov-ind-19-data/", latest, "/plot4a.RDS"))
+p4b <- readRDS(paste0("~/cov-ind-19-data/", latest, "/plot4b.RDS"))
+p5a <- readRDS(paste0("~/cov-ind-19-data/", latest, "/plot5a.RDS"))
+p5b <- readRDS(paste0("~/cov-ind-19-data/", latest, "/plot5b.RDS"))
+p6a <- readRDS(paste0("~/cov-ind-19-data/", latest, "/plot6a.RDS"))
+p6b <- readRDS(paste0("~/cov-ind-19-data/", latest, "/plot6b.RDS"))
+
+
+save(p1, p2, p3, p4a, p4b, p5a, p5b, p6a, p6b,
+     file = paste0("~/cov-ind-19-data/", latest, "/plots.RData")
+)
