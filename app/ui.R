f3238023c3dd46227f326e53b63bc45c5acc4fa5
Max Salvatore <38917700+maxsal@users.noreply.github.com>
2020-04-03 18:19:29 -0400

    Update ui.R

--- app/ui.R
+++ app/ui.R
@@ -90,12 +90,12 @@ shinyUI(fluidPage(
         tags$ol(
           tags$li("No intervention"),
           tags$li("Social distancing and travel ban (without March 25 lockdown)"),
-          tags$li("Lockdown until April 15 with a gradual, moderate resumption of daily activities")
+          tags$li("Lockdown until April 14 with a gradual, moderate resumption of daily activities")
         ),
         "Because we are using SIR models to generate the forecast, we explicitly delay changes in the basic reproduction number one week (Figure 4a) and two weeks (Figure 4b) to capture delayed onset of cases due to incubation period and timeliness of adherence.",
         "In general terms, the one-week delay models (Figures 4a, 5a, and 5b) can be thought of as 'quick adherence' to the lockdown measures, while the 'two-week' delay models (Figures 4b, 6a, and 6b) can be though of as 'slow adherence' to the lockdown measures.",
         HTML(paste0("All models assume a basic reproduction number of 2 under no intervention. The implied R", tags$sub("0"), " is 1.5 under Scenario 2.")),
-        HTML(paste0("We further assume the R", tags$sub("0"), " drops to 0.8 under lockdown and then gradually rises back up to 1.5 after the lockdown ends over a three week period ('lockdown with moderate release').")),
+        HTML(paste0("We further assume the R", tags$sub("0"), " drops to 0.8 under lockdown and then gradually rises back up to 1.5 after the lockdown ends over a three week period ('lockdown with moderate return').")),
         "You can hover of the bars for dates and counts. Also, please note the dotted line represents the upper confidence interval for the lockdown scenario (3), which is closest to the current intervention.",
         "Our codes are available on ", a("GitHub", .noWS = "outside", href = "https://github.com/umich-cphds/cov-ind-19") ," and so users can change the nature of interventions."),
       h2("Quick adherence (one-week delay)"),
