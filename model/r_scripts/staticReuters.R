5f122e19c483523d0e3f9cb01a4c12ea29b4e524
Kleinsasser <mkleinsa@UMICH.EDU>
2020-04-13 12:48:02 -0400

    widen list of included countries, small tweaks

--- model/r_scripts/staticReuters.R
+++ model/r_scripts/staticReuters.R
@@ -4,19 +4,25 @@ library(vroom)
 library(ggplot2)
 
 start.date = as.Date("2020-03-01")
-today <- Sys.getenv("today")
+today <- "2020-04-12"
 
-data <- vroom(paste0("~/cov-ind-19-data/", today, "/jhu_data.csv")) %>%
-  group_by(Country) %>% filter(Case >= 100) %>%
+data <- vroom(paste0("C:/Users/mkleinsa/Box/Projects/covid-india/ndb/cov-ind-19-data/new/jhu_data.csv")) %>%
+  group_by(Country) %>% filter(Cases >= 100) %>%
   arrange(Date) %>%
   mutate(Day = seq(n()))
 
 Day.max <- 60 # nrow(data %>% filter(Country == "India"))
+max.plot.day = as.Date("2020-04-10", format = "%Y-%m-%d")
+data = 
+  data %>% filter(Date < max.plot.day)
+
 data <- filter(data, Day <= Day.max) %>%
   mutate(Date = format(Date, format = "%b %e")) %>%
   ungroup() %>%
-  mutate(num.fmt = format(Case, big.mark = ",", scientific = F, trim = T)) %>%
+  mutate(num.fmt = format(Cases, big.mark = ",", scientific = F, trim = T)) %>%
   mutate(text = paste0(Date, ": ", num.fmt, " cumulative cases"))
+
+
 
 # title <- paste("Cumulative number of COVID-19 cases in India compared",
 #                "to other countries affected by the pandemic")
@@ -25,14 +31,21 @@ data <- filter(data, Day <= Day.max) %>%
 #               format(today, format = "%b %e"), sep = ' ')
 
 colors <- c(
-  "China"       = "#ED553B",
-  "South Korea" = "#56738A",
-  "Italy"       = "#0472CF",
-  "Iran"        = "#173F5F",
-  "France"      = "#3CAEA3",
-  "Germany"     = "#f2c82e",
-  "US"          = "#822F21",
-  "India"       = "#138808"
+  "China"       = "#003366",
+  "South Korea" = "#3366cc",
+  "Italy"       = "#666699",
+  "Iran"        = "#9900ff",
+  "France"      = "#660066",
+  "Germany"     = "#990033",
+  "US"          = "#996600",
+  "India"       = "#138808",
+  "Spain" = "#999966",
+  "United Kingdom" = "#009999",
+  "Turkey" = "#0099ff",
+  "Belgium" = "#9999ff",
+  "Netherlands" = "#ff6699",
+  "Switzerland" = "#ffcc66",
+  "Canada" = "#ccccff"
 )
 
 ymax        <- log10(1e6)
@@ -43,16 +56,16 @@ db = function(x, intercept, double) {
 }
 
 p1 = 
-ggplot(data = data, aes(x = Day, y = log(Case, base = 10), color = Country)) + 
-  geom_line(size = 1.0, alpha = 0.8) + 
+ggplot(data = data %>% filter(Country != "India"), aes(x = Day, y = log(Cases, base = 10), color = Country)) + 
+  geom_line(size = 1.0, alpha = 0.75) + 
   theme_bw() + 
   theme(panel.background = element_rect(fill = NA),
         panel.ontop = FALSE,
         panel.border = element_blank(),
-        legend.position = c(.15, .95),
+        legend.position = c(.13, .95),
         legend.justification = c("right", "top"),
         legend.box.just = "left",
-        legend.margin = margin(2, 2, 2, 2),
+        legend.margin = margin(1, 1, 1, 1),
         legend.background=element_blank(),
         axis.text.x = element_text(size = 12),
         axis.text.y = element_text(size = 12),
@@ -61,16 +74,17 @@ ggplot(data = data, aes(x = Day, y = log(Case, base = 10), color = Country)) +
         axis.line = element_line(size = 1, colour = "black"), 
         axis.line.y.right = element_blank(), 
         axis.ticks.y.right = element_blank()) + 
+  geom_line(data = data %>% filter(Country == "India"), size = 2.0, alpha = 0.8) + 
   stat_function(fun = db, args = list(intercept = 100, double = 3), xlim = c(2, 60), 
-                color = '#545451', size = 0.7, linetype = "f8", alpha = 0.75) +
-  annotate("text", x = 36.9, y = 5.63, label = "3 days", angle = 45, color = '#545451') + 
+                color = '#363534', size = 0.8, linetype = "f8", alpha = 0.75) +
+  annotate("text", x = 39.2, y = 5.86, label = "3 days", angle = 47, color = '#545451') + 
   stat_function(fun = db, args = list(intercept = 100, double = 6), xlim = c(2, 60), 
-                color = '#545451', size = 0.7, linetype = "f8", alpha = 0.75) + 
-  annotate("text", x = 55, y = 4.7, label = "6 days", angle = 28, color = '#545451') + 
+                color = '#363534', size = 0.8, linetype = "f8", alpha = 0.75) + 
+  annotate("text", x = 55, y = 4.7, label = "6 days", angle = 29, color = '#545451') + 
   stat_function(fun = db, args = list(intercept = 100, double = 14), xlim = c(2, 60), 
-                color = '#545451', size = 0.7, linetype = "f8", alpha = 0.75) + 
-  annotate("text", x = 55, y = 3.12, label = "14 days", angle = 13, color = '#545451') + 
-  ylim(c(2, log(max(data$Case) + 1000000, base = 10))) + 
+                color = '#363534', size = 0.8, linetype = "f8", alpha = 0.75) + 
+  annotate("text", x = 55, y = 3.13, label = "14 days", angle = 13, color = '#545451') + 
+  ylim(c(2, log(max(data$Cases) + 1000000, base = 10))) + 
   scale_color_manual(values=c(colors)) + 
   xlab('Days since cases reached 100') + 
   ylab('Cumulative number of cases') + 
