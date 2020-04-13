226108e1a0858777a273dc5f1af4668e189f6d0e
Dan Barker <danbarke@umich.edu>
2020-04-13 18:38:25 -0400

    Add 2 new one_off scripts

---
+++ model/r_scripts/one_off/forecasting_social_dist_cluster.R
@@ -0,0 +1,118 @@
+# libraries ----------
+library(tidyverse)
+library(chron)
+library(rjags)
+library(gtools) #rdirichlet(n, alpha)
+library(here)
+library(glue)
+library(devtools)
+
+arrayid=Sys.getenv("SLURM_ARRAY_TASK_ID")
+set.seed(20192020) # default: 20192020
+
+# specificatioons ----------
+length_of_lockdown <- 35            # in days (default = 35)
+Ms                 <- 5e3           # 5e5 recommended (5e3 for testing - but not stable)
+nburnins           <- 2e3           # 2e5 recommended (2e3 for testing - but not stable)
+pi_sdtb            <- 0.75          # pi corresponding to social distancing and travel ban
+R_0                <- 2             # basic reproduction number
+save_mcmc          <- TRUE          # output MCMC files (default = TRUE; needed for incidence CI calculations)
+start_date         <- "2020-03-01"
+soc_dist_date      <- "2020-03-24"
+
+# eSIR ----------
+source_url("https://github.com/lilywang1988/eSIR/blob/master/R/tvt.eSIR.R?raw=TRUE") # relevant model code
+
+# directory ----------
+today <- Sys.getenv("today")
+
+# data ----------
+dat <- read_tsv(url("https://raw.githubusercontent.com/umich-cphds/cov-ind-19-data/master/2020-04-13/jhu_data_mod.csv")) %>%
+  filter(Country == "India" &  Date >= "2020-03-01" & Date <= soc_dist_date)
+
+NI_complete <- dat$Cases
+RI_complete <- dat$Recovered + dat$Deaths
+N           <- 1.34e9                          # population of India
+R           <- unlist(RI_complete/N)           # proportion of recovered per day
+Y           <- unlist(NI_complete/N-R)
+
+# models ---------
+if (arrayid == 1) {
+# ------------- # 
+# DAN LOOK HERE #
+# ------------- # 
+wd <- paste0("~/cov-ind-19-data/", today, "/1wk/")
+if (!dir.exists(wd)) {
+  dir.create(wd, recursive = TRUE)
+  message("Creating ", wd)
+}
+setwd(wd)
+
+delay <- 7
+
+print(glue("Running model_2 (perpetual social distancing and travel ban) with {delay/7} week delay"))
+
+change_time <- format(c(as.Date((as.Date("2020-03-15") + delay):(as.Date("2020-03-15") + delay + 13), origin = "1970-01-01"),
+                        as.Date(as.Date("2020-03-15") + delay + 14, origin = "1970-01-01")), "%m/%d/%Y")
+pi0         <- c(1,
+                 rev(seq(pi_sdtb, 1, (1 - pi_sdtb) / 14))[-1],
+                 pi_sdtb)
+
+soc_dist <- tvt.eSIR(
+  Y,
+  R,
+  begin_str      = "03/01/2020",
+  death_in_R     = 0.2,
+  T_fin          = 200,
+  pi0            = pi0,
+  change_time    = change_time,
+  R0             = R_0,
+  dic            = TRUE,
+  casename       = "India_2",
+  save_files     = FALSE,
+  save_mcmc      = save_mcmc,
+  save_plot_data = TRUE,
+  M              = Ms,
+  nburnin        = nburnins
+)
+}
+
+if (arrayid == 2) {
+# ------------- # 
+# DAN LOOK HERE #
+# ------------- # 
+wd <- paste0("~/cov-ind-19-data/", today, "/1wk/")
+if (!dir.exists(wd)) {
+  dir.create(wd, recursive = TRUE)
+  message("Creating ", wd)
+}
+setwd(wd)
+
+delay <- 14
+
+print(glue("Running model_2 (perpetual social distancing and travel ban) with {delay/7} week delay"))
+
+change_time <- format(c(as.Date((as.Date("2020-03-15") + delay):(as.Date("2020-03-15") + delay + 13), origin = "1970-01-01"),
+                        as.Date(as.Date("2020-03-15") + delay + 14, origin = "1970-01-01")), "%m/%d/%Y")
+pi0         <- c(1,
+                 rev(seq(pi_sdtb, 1, (1 - pi_sdtb) / 14))[-1],
+                 pi_sdtb)
+
+soc_dist <- tvt.eSIR(
+  Y,
+  R,
+  begin_str      = "03/01/2020",
+  death_in_R     = 0.2,
+  T_fin          = 200,
+  pi0            = pi0,
+  change_time    = change_time,
+  R0             = R_0,
+  dic            = TRUE,
+  casename       = "India_2",
+  save_files     = FALSE,
+  save_mcmc      = save_mcmc,
+  save_plot_data = TRUE,
+  M              = Ms,
+  nburnin        = nburnins
+)
+}
---
+++ model/r_scripts/one_off/forecasting_travel_ban_cluster.R
@@ -0,0 +1,63 @@
+# libraries ----------
+library(tidyverse)
+library(chron)
+library(rjags)
+library(gtools) #rdirichlet(n, alpha)
+library(scales) #alpha　function
+library(data.table)
+library(devtools)
+
+arrayid=Sys.getenv("SLURM_ARRAY_TASK_ID")
+set.seed(20192020) # default: 20192020
+
+# specificatioons ----------
+length_of_lockdown <- 35            # in days (default = 35)
+Ms                 <- 5e3           # 5e5 recommended (5e3 for testing - but not stable)
+nburnins           <- 2e3           # 2e5 recommended (2e3 for testing - but not stable)
+R_0                <- 2             # basic reproduction number
+save_mcmc          <- TRUE          # output MCMC files (default = TRUE; needed for incidence CI calculations)
+start_date         <- "2020-03-01"
+travel_ban_date    <- "2020-03-15"
+
+# eSIR ----------
+source_url("https://github.com/lilywang1988/eSIR/blob/master/R/tvt.eSIR.R?raw=TRUE") # relevant model code
+
+# directory ----------
+today <- Sys.getenv("today")
+
+# ------------- # 
+# DAN LOOK HERE #
+# ------------- # 
+wd <- paste0("~/cov-ind-19-data/", today, "/1wk/")
+if (!dir.exists(wd)) {
+  dir.create(wd, recursive = TRUE)
+  message("Creating ", wd)
+}
+setwd(wd)
+
+# data ----------
+dat <- read_tsv(url("https://raw.githubusercontent.com/umich-cphds/cov-ind-19-data/master/2020-04-13/jhu_data_mod.csv")) %>%
+  filter(Country == "India" &  Date >= "2020-03-01" & Date <= travel_ban_date)
+
+NI_complete <- dat$Cases
+RI_complete <- dat$Recovered + dat$Deaths
+N           <- 1.34e9                          # population of India
+R           <- unlist(RI_complete/N)           # proportion of recovered per day
+Y           <- unlist(NI_complete/N-R)
+
+# models ---------
+travel_ban <- tvt.eSIR(
+  Y,
+  R,
+  begin_str      = "03/01/2020",
+  death_in_R     = 0.2,
+  T_fin          = 200,
+  R0             = R_0,
+  dic            = TRUE,
+  casename       = "India_3",
+  save_files     = FALSE,
+  save_mcmc      = save_mcmc,
+  save_plot_data = TRUE,
+  M              = Ms,
+  nburnin        = nburnins
+)
