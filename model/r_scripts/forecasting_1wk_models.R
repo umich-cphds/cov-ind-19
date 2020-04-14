3a7900e46903b22763299e673f729224798234b3
Dan Barker <danbarke@umich.edu>
2020-04-13 21:48:05 -0400

    Update national forecasts

--- model/r_scripts/forecasting_1wk_models.R
+++ model/r_scripts/forecasting_1wk_models.R
@@ -1,189 +1,80 @@
 # libraries ----------
 library(tidyverse)
 library(chron)
-library(reshape2)
 library(rjags)
 library(gtools) #rdirichlet(n, alpha)
-library(scales) #alpha　function
-library(data.table)
+library(here)
+library(glue)
 library(devtools)
 
 # Set variables based on testing or production
-if ( Sys.getenv("production") == "TRUE" ) {
-	data_repo <- "~/cov-ind-19-data/"
-	Ms        <- 5e5    # 5e5 recommended (5e3 for testing - but not stable)
-	nburnins  <- 2e5    # 2e5 recommended (2e3 for testing - but not stable)
-	today     <- Sys.getenv("today")
+if ( Sys.getenv("production") == "TRUE" ) { 
+        data_repo <- "~/cov-ind-19-data/"
+        Ms        <- 5e5    # 5e5 recommended (5e3 for testing - but not stable)
+        nburnins  <- 2e5    # 2e5 recommended (2e3 for testing - but not stable)
+        today     <- Sys.getenv("today")
 } else {
-	data_repo <- "~/cov-ind-19-test/"
-	Ms        <- 5e3    # 5e5 recommended (5e3 for testing - but not stable)
-	nburnins  <- 2e3    # 2e5 recommended (2e3 for testing - but not stable)
-	today     <- max(as.Date(grep("[0-9]", list.files(data_repo), value = T)))
+        data_repo <- "~/cov-ind-19-test/"
+        Ms        <- 5e3    # 5e5 recommended (5e3 for testing - but not stable)
+        nburnins  <- 2e3    # 2e5 recommended (2e3 for testing - but not stable)
+        today     <- max(as.Date(grep("[0-9]", list.files(data_repo), value = T)))
 }
 
 arrayid=Sys.getenv("SLURM_ARRAY_TASK_ID")
 set.seed(20192020) # default: 20192020
-R_0      <- 2      # basic reproduction number
+
+# specificatioons ----------
+delay              <- 7             # in days (default = 7)
+length_of_lockdown <- 35            # in days (default = 21)
+pi_cautious        <- 0.6           # pi corresponding to cautious return
+pi_lockdown        <- 0.4           # pi corresponding to lockdown
+pi_moderate        <- 0.75          # pi corresponding to moderate return
+pi_normal          <- 1             # pi corresponding to normal (pre-intervention) return
+pi_sdtb            <- 0.75          # pi corresponding to social distancing and travel ban
+R_0                <- 2             # basic reproduction number
+save_mcmc          <- FALSE          # output MCMC files (default = TRUE; needed for incidence CI calculations)
+speed_lockdown     <- 7             # length of time for lockdown to drop (in days)
+speed_return       <- 21            # length of time for pi to return to post-lockdown pi (in days)
+start_date         <- "2020-03-01"
+soc_dist_start     <- "2020-03-15"
+soc_dist_end       <- "2020-03-24"
+lockdown_start     <- as.Date(soc_dist_end) + 1
 
 # eSIR ----------
 source_url("https://github.com/lilywang1988/eSIR/blob/master/R/tvt.eSIR.R?raw=TRUE") # relevant model code
 
-# !! directory ----------
+# directory ----------
 wd <- paste0(data_repo, today, "/1wk/")
 if (!dir.exists(wd)) {
-    dir.create(wd, recursive = TRUE)
-    message("Creating ", wd)
+  dir.create(wd, recursive = TRUE)
+  message("Creating ", wd)
 }
 setwd(wd)
 
 # data ----------
-jhu_cases     <- read.csv("~/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
-jhu_recovered <- read.csv("~/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv") # pull data daily
-jhu_deaths    <- read.csv("~/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
+dat <- read_tsv(url(glue("{data_repo_path}/{today}/jhu_data_mod.csv"))) %>%
+  filter(Country == "India" &  Date >= "2020-03-01")
 
-### check that datasets end on same date ----------
-  last_jhuc_case_date     <- as.Date(sub("X", "", rev(names(jhu_cases))[1]), format("%m.%d.%y"))
-  last_jhur_case_date     <- as.Date(sub("X", "", rev(names(jhu_recovered))[1]), format("%m.%d.%y"))
-  last_jhud_case_date     <- as.Date(sub("X", "", rev(names(jhu_deaths))[1]), format("%m.%d.%y"))
-  min_date <- min(c(last_jhuc_case_date, last_jhur_case_date, last_jhud_case_date))
-
-  india_recovered <- jhu_recovered %>%
-    filter(Country.Region == "India") %>%
-    select(-c("Province.State", "Lat", "Long")) %>%
-    gather(key = "date", value = "count", -Country.Region) %>%
-    mutate(date = as.Date(sub("X", "", date), format = "%m.%d.%y")) %>%
-    filter(date >= "2020-03-01" & date <= min_date )
-
-  india_deaths <- jhu_deaths %>%
-    filter(Country.Region == "India") %>%
-    select(-c("Province.State", "Lat", "Long")) %>%
-    gather(key = "date", value = "count", -Country.Region) %>%
-    mutate(date = as.Date(sub("X", "", date), format = "%m.%d.%y")) %>%
-    filter(date >= "2020-03-01" & date <= min_date )
-
-  india_cases <- jhu_cases %>%
-    filter(Country.Region == "India") %>%
-    select(-c("Province.State", "Lat", "Long")) %>%
-    gather(key = "date", value = "count", -Country.Region) %>%
-    mutate(date = as.Date(sub("X", "", date), format = "%m.%d.%y")) %>%
-    filter(date >= "2020-03-01" & date <= min_date )
-
-NI_complete <- india_cases$count
-RI_complete <- india_recovered$count + india_deaths$count
+NI_complete <- dat$Cases
+RI_complete <- dat$Recovered + dat$Deaths
 N           <- 1.34e9                          # population of India
 R           <- unlist(RI_complete/N)           # proportion of recovered per day
 Y           <- unlist(NI_complete/N-R)
 
+change_time <- format(c(as.Date((as.Date(soc_dist_start) + delay):(as.Date(soc_dist_end) + delay), origin = "1970-01-01"),
+                        as.Date((as.Date(lockdown_start) + delay):(as.Date(lockdown_start) + delay + speed_lockdown), origin = "1970-01-01"),
+                        as.Date((as.Date(lockdown_start) + delay + length_of_lockdown):(as.Date(lockdown_start) + delay + length_of_lockdown + speed_return), origin = "1970-01-01")), "%m/%d/%Y")
+
+l <- length(as.Date((as.Date(soc_dist_start) + delay):(as.Date(soc_dist_end) + delay), origin = "1970-01-01"))
+
 # models ---------
-
-# --------------------------------------------------------------------- #
-# all model parameters are 1 week ahead of those stated in the scenario #
-# --------------------------------------------------------------------- #
-
 if (arrayid == 1) {
-print("Running model_1")
-# Model 1 ----------
-# Model 1 Scenario: Travel ban + social distancing + lockdown
-#   - March 1 - March 25:  1
-#   - March 25 - April 8:  1 > 0.75
-#   - April 8 - April 15:  0.75 > 0.4
-#   - April 29 - May 20:   0.4
-change_time <- c(format(seq(as.Date("2020/03/25") - 7, as.Date("2020/04/07") - 7, "days"), "%m/%d/%Y"),
-                 format(seq(as.Date("2020/04/08") - 7, as.Date("2020/04/15") - 7, "days"), "%m/%d/%Y"))
+print(glue("Running model_4 (lockdown with moderate return) with {delay/7} week delay and {length_of_lockdown}-day lockdown"))
 pi0         <- c(1,
-                 rev(seq(0.75, 1, 0.25 / 14))[-1],
-                 rev(seq(0.4, 0.75, 0.35 / 7))[-1],
-                 0.4)
-
-model_1 <- tvt.eSIR(
-  Y,
-  R,
-  begin_str      = "03/01/2020",
-  death_in_R     = 0.2,
-  T_fin          = 200,
-  pi0            = pi0,
-  change_time    = change_time,
-  R0             = R_0,
-  dic            = TRUE,
-  casename       = "India_1",
-  save_files     = FALSE,
-  save_mcmc      = FALSE,
-  save_plot_data = TRUE,
-  M              = Ms,
-  nburnin        = nburnins
-)
-}
-
-if(arrayid == 2){
-print("Running model_2")
-# Model 2 ----------
-# Model 2 Scenario: Travel ban + social distancing
-#   - March 1 - March 25:  1
-#   - March 25 - April 8:  1 > 0.75
-change_time <- c(format(seq(as.Date("2020/03/25") - 7, as.Date("2020/04/07") - 7, "days"), "%m/%d/%Y"),
-                 format(as.Date("2020/04/08") - 7, "%m/%d/%Y"))
-pi0         <- c(1,
-                 rev(seq(0.75, 1, 0.25 / 14))[-1],
-                 0.75)
-
-model_2 <- tvt.eSIR(
-  Y,
-  R,
-  begin_str      = "03/01/2020",
-  death_in_R     = 0.2,
-  T_fin          = 200,
-  pi0            = pi0,
-  change_time    = change_time,
-  R0             = R_0,
-  dic            = TRUE,
-  casename       = "India_2",
-  save_files     = FALSE,
-  save_mcmc      = FALSE,
-  save_plot_data = TRUE,
-  M              = Ms,
-  nburnin        = nburnins
-)
-}
-
-if(arrayid==3){
-print("Running model_3")
-# Model 3 ----------
-# Model 3 Scenario: No intervention
-model_3 <- tvt.eSIR(
-  Y,
-  R,
-  begin_str      = "03/01/2020",
-  death_in_R     = 0.2,
-  T_fin          = 200,
-  R0             = R_0,
-  dic            = TRUE,
-  casename       = "India_3",
-  save_files     = FALSE,
-  save_mcmc      = FALSE,
-  save_plot_data = TRUE,
-  M              = Ms,
-  nburnin        = nburnins
-)
-}
-
-if(arrayid==4){
-print("Running model_4")
-# Model 4 ----------
-# Model 4 Scenario: Travel ban + social distancing + lockdown,
-#                   then release lockdown (average)
-#   - March 1 - March 25:  1
-#   - March 25 - April 8:  1 > 0.75
-#   - April 8 - April 15:  0.75 > 0.4
-#   - April 29 - May 20:   0.4 > 0.6
-#   - May 20:              0.6
-change_time <- c(format(seq(as.Date("2020/03/25") - 7, as.Date("2020/04/07") - 7, "days"), "%m/%d/%Y"),
-                 format(seq(as.Date("2020/04/08") - 7, as.Date("2020/04/15") - 7, "days"), "%m/%d/%Y"),
-                 format(seq(as.Date("2020/04/29") - 7, as.Date("2020/05/20") - 7, "days"), "%m/%d/%Y"))
-pi0         <- c(1,
-                 rev(seq(0.75, 1, 0.25 / 14))[-1],
-                 rev(seq(0.4, 0.75, 0.35 / 7))[-1],
-                 seq(0.4, 0.75, 0.35 / 21),
-                 0.75)
+                 rev(seq(pi_sdtb, 1, (1-pi_sdtb) / l))[-1],
+                 rev(seq(pi_lockdown, pi_sdtb, (pi_sdtb-pi_lockdown) / speed_lockdown))[-1],
+                 seq(pi_lockdown, pi_moderate, (pi_moderate - pi_lockdown) / speed_return),
+                 pi_moderate)
 
 model_4 <- tvt.eSIR(
   Y,
@@ -197,31 +88,20 @@ model_4 <- tvt.eSIR(
   dic            = TRUE,
   casename       = "India_4",
   save_files     = FALSE,
-  save_mcmc      = FALSE,
+  save_mcmc      = save_mcmc,
   save_plot_data = TRUE,
   M              = Ms,
   nburnin        = nburnins
 )
 }
 
-if(arrayid==5){
-print("Running model_5")
-# Model 5 ----------
-# Model 5 Scenario: Travel ban + social distancing + lockdown,
-#                   then release lockdown (party)
-#   - March 1 - March 25:  1
-#   - March 25 - April 8:  1 > 0.75
-#   - April 8 - April 15:  0.75 > 0.4
-#   - April 29 - May 20:   0.4 > 1
-#   - May 20:              1
-change_time <- c(format(seq(as.Date("2020/03/25") - 7, as.Date("2020/04/07") - 7, "days"), "%m/%d/%Y"),
-                 format(seq(as.Date("2020/04/08") - 7, as.Date("2020/04/15") - 7, "days"), "%m/%d/%Y"),
-                 format(seq(as.Date("2020/04/29") - 7, as.Date("2020/05/20") - 7, "days"), "%m/%d/%Y"))
+if (arrayid == 2) {
+print(glue("Running model_5 (lockdown with normal [pre-intervention] return) with {delay/7} week delay and {length_of_lockdown}-day lockdown"))
 pi0         <- c(1,
-                 rev(seq(0.75, 1, 0.25 / 14))[-1],
-                 rev(seq(0.4, 0.75, 0.35 / 7))[-1],
-                 seq(0.4, 1, 0.6 / 21),
-                 1)
+                 rev(seq(pi_sdtb, 1, (1-pi_sdtb) / l))[-1],
+                 rev(seq(pi_lockdown, pi_sdtb, (pi_sdtb-pi_lockdown) / speed_lockdown))[-1],
+                 seq(pi_lockdown, pi_normal, (pi_normal - pi_lockdown) / speed_return),
+                 pi_normal)
 
 model_5 <- tvt.eSIR(
   Y,
@@ -235,31 +115,20 @@ model_5 <- tvt.eSIR(
   dic            = TRUE,
   casename       = "India_5",
   save_files     = FALSE,
-  save_mcmc      = FALSE,
+  save_mcmc      = save_mcmc,
   save_plot_data = TRUE,
   M              = Ms,
   nburnin        = nburnins
 )
 }
 
-if(arrayid==6){
-print("Running model_6")
-# Model 6 ----------
-# Model 6 Scenario: Travel ban + social distancing + lockdown,
-#                   then release lockdown (scared)
-#   - March 1 - March 25:  1
-#   - March 25 - April 8:  1 > 0.75
-#   - April 8 - April 15:  0.75 > 0.4
-#   - April 29 - May 20:   0.4 > 0.6
-#   - May 20:              0.6
-change_time <- c(format(seq(as.Date("2020/03/25") - 7, as.Date("2020/04/07") - 7, "days"), "%m/%d/%Y"),
-                 format(seq(as.Date("2020/04/08") - 7, as.Date("2020/04/15") - 7, "days"), "%m/%d/%Y"),
-                 format(seq(as.Date("2020/04/29") - 7, as.Date("2020/05/20") - 7, "days"), "%m/%d/%Y"))
+if (arrayid == 3) {
+print(glue("Running model_6 (lockdown with cautious return) with {delay/7} week delay and {length_of_lockdown}-day lockdown"))
 pi0         <- c(1,
-                 rev(seq(0.75, 1, 0.25 / 14))[-1],
-                 rev(seq(0.4, 0.75, 0.35 / 7))[-1],
-                 seq(0.4, 0.6, 0.2 / 21),
-                 0.6)
+                 rev(seq(pi_sdtb, 1, (1-pi_sdtb) / l))[-1],
+                 rev(seq(pi_lockdown, pi_sdtb, (pi_sdtb-pi_lockdown) / speed_lockdown))[-1],
+                 seq(pi_lockdown, pi_cautious, (pi_cautious - pi_lockdown) / speed_return),
+                 pi_cautious)
 
 model_6 <- tvt.eSIR(
   Y,
@@ -273,7 +142,7 @@ model_6 <- tvt.eSIR(
   dic            = TRUE,
   casename       = "India_6",
   save_files     = FALSE,
-  save_mcmc      = FALSE,
+  save_mcmc      = save_mcmc,
   save_plot_data = TRUE,
   M              = Ms,
   nburnin        = nburnins
