4860e52ed3efca78c8f33728b8601b125fc5d01c
Alexander Rix <alexanderjrix@gmail.com>
2020-04-15 17:20:56 -0400

    generate spatial map using covid19india.org data

--- model/r_scripts/spatialmap.R
+++ model/r_scripts/spatialmap.R
@@ -28,63 +28,22 @@ kalends.ides <- as.Date(paste0(c(paste0("2020-", 1:12), paste0("2020-", 1:12)),
 # shape file for spatial plot
 india_shp   <- st_read("~/cov-ind-19/model/map/Indian_States.shp")
 
-# authenticate as alexander rix and pull the latest data from the kaggle dataset.
-kaggle.auth <- read_json("~/cov-ind-19/model/map/.kaggle.json")
 
-auth    <- authenticate(kaggle.auth$user, kaggle.auth$key)
-url     <- "https://www.kaggle.com/api/v1/datasets/download/sudalairajkumar/covid19-in-india/covid_19_india.csv"
-request <- GET(url, auth)
-
-# Check for errors
-stop_for_status(request)
-if (http_type(request) != "text/csv")
-    stop(header$date, ": GET did not result in the correct content type.")
-
-
-col_types <- cols(
-    Sno  = col_double(),
-    Date = col_date(format = "%d/%m/%y"),
-    Time = col_time(),
-    `State/UnionTerritory`   = col_character(),
-    ConfirmedIndianNational  = col_character(),
-    ConfirmedForeignNational = col_character(),
-    Cured     = col_double(),
-    Deaths    = col_double(),
-    Confirmed = col_double()
+data <- vroom(paste0("~/cov-ind-19-data/", today, "/covid19india_data.csv")) %>%
+arrange(Name, Date) %>%
+mutate(State = case_when(
+    Name == "Ladakh" ~ "Jammu & Kashmir",
+    TRUE ~ State)
+) %>%
+filter(Date %in% c(kalends.ides, seq(max(Date) - days.back, max(Date), 1)) &
+       Date >= start.date
 )
 
-data <- content(request, col_types = col_types) %>%
-    select(State = `State/UnionTerritory`, Cases = Confirmed, Date) %>%
-    arrange(State, Date) %>%
-    mutate(State = case_when(
-        State == "Delhi" ~ "NCT of Delhi",
-        State == "Telengana" ~ "Telangana",
-        State == "Chattisgarh" ~ "Chandigarh",
-        State == "Pondicherry" ~ "Puducherry",
-        State == "Andaman and Nicobar Islands" ~ "Andaman & Nicobar Island",
-        State == "Jammu and Kashmir" ~ "Jammu & Kashmir",
-        State == "Ladakh" ~ "Jammu & Kashmir",
-        TRUE ~ State)
-
-    # Black Magick
-    ) %>% (function (x)
-        add_row(x, State = setdiff(india_shp$st_nm, x$State), Date = start.date,
-                Cases = NA)
-    ) %>%
-    mutate(State = as.factor(State)) %>%
-    group_by(State, Date) %>%
-    summarise(Cases = sum(Cases)) %>%
-    ungroup() %>%
-    spread(Date, Cases, fill = NA) %>%
-    gather(matches("[0-9].+"), key = Date, value = Cases) %>%
-    mutate(Date = as.Date(Date)) %>%
-    filter(Date %in% c(kalends.ides, seq(max(Date) - days.back, max(Date), 1)) &
-           Date >= start.date)
-
-i <- match(data$State, india_shp$st_nm)
+i <- match(data$Name, india_shp$st_nm)
 
 final_data       <- india_shp[i, ]
 final_data$Cases <- data$Cases
+final_data$Cases <- ifelse(final_data$Cases == 0, NA, final_data$Cases)
 final_data$Text <- as.character(ifelse(is.na(data$Cases), 0, data$Cases))
 final_data$Date  <- data$Date
 # final_data$Date <- as.factor(format.Date(data$Date, format("%e %b %Y")))
