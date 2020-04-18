439fc1b15d0f69f373ca3dae2e0476509021abac
Alexander Rix <alexanderjrix@gmail.com>
2020-04-18 11:21:40 -0400

    update mike plots

--- model/r_scripts/plots/plot_fig_8.R
+++ model/r_scripts/plots/plot_fig_8.R
@@ -1,9 +1,15 @@
+# Set variables based on testing or production
+if (Sys.getenv("production") == "TRUE") {
+    data_repo <- "~/cov-ind-19-data/"
+    today     <- Sys.getenv("today")
+} else {
+    data_repo <- "~/cov-ind-19-test/"
+    today     <- max(as.Date(grep("[0-9]", list.files(data_repo), value = T)))
+}
 
-plot_fig_8 = function(start.date = as.Date('2020-04-01')) {
-  
-  data <- vroom(paste0(data_repo, today, "/testing.csv"))
-  #today = '2020-04-17' # delete this later
-  data = vroom('C:/Users/mkleinsa/Box/Projects/covid-india/IRIS5/cov-ind-19-data/today/testing.csv') %>%
+plot_fig_8 <- function(start.date = as.Date('2020-04-01'))
+{
+    data <- vroom(paste0(data_repo, today, "/testing.csv")) %>%
     filter(Country == "India") %>%
     filter(Date >= start.date) %>%
     mutate(Date = as.factor(format(Date, format = "%b %e")),
@@ -12,42 +18,49 @@ plot_fig_8 = function(start.date = as.Date('2020-04-01')) {
            Text_cases = paste0(Date, ": ", Cases, ' cases'),
            Text_tests = paste0(Date, ": ", Tests, ' tests')) %>%
     drop_na()
-  
-  data = 
-  bind_rows(data %>% select(-Tests, -Text_tests) %>% mutate(Count = Cases, Text = Text_cases, Type = 'Cases'),
-            data %>% select(-Cases, -Text_cases) %>% mutate(Count = Tests, Text = Text_tests, Type = 'Tests')) %>%
+
+  data <- bind_rows(
+      data %>%
+      select(-Tests, -Text_tests) %>%
+      mutate(
+          Count = Cases,
+          Text = Text_cases,
+          Type = 'Cases'),
+          data %>%
+          select(-Cases, -Text_cases) %>%
+          mutate(Count = Tests, Text = Text_tests, Type = 'Tests')
+    ) %>%
     select(Date, Count, Text, Type, Country)
-  
-  cap <- paste0("© COV-IND-19 Study Group. Last updated: ",
-                format(today, format = "%b %e"), sep = ' ')
-  
-  title <- paste("Daily confirmed positive cases and total tests in India")
-  
-  axis.title.font <- list(size = 16)
-  tickfont        <- list(size = 16)
-  
-  xaxis <- list(title = "", titlefont = axis.title.font, showticklabels = TRUE,
-                tickangle = -30, zeroline = F)
-  
-  yaxis <- list(title = "Daily counts", titlefont = axis.title.font,
-                tickfont = tickfont, zeroline = T)
-  colors <- c(
-    "Cases" = "#ED553B",
-    "Tests"  = "#f2c82e"
-  )
-  
-  p <- plot_ly(data, x = ~Date, y = ~Count, color = ~Type, text = ~Text,
-               type = "bar", colors = colors, hoverinfo = "text"
-  ) %>%
+
+    cap <- paste0("© COV-IND-19 Study Group. Last updated: ",
+                  format(today, format = "%b %e"), sep = ' ')
+
+    title <- paste("Daily confirmed positive cases and total tests in India")
+
+    axis.title.font <- list(size = 16)
+    tickfont        <- list(size = 16)
+
+    xaxis <- list(title = "", titlefont = axis.title.font, showticklabels = TRUE,
+                  tickangle = -30, zeroline = F)
+
+    yaxis <- list(title = "Daily counts", titlefont = axis.title.font,
+                  tickfont = tickfont, zeroline = T)
+    colors <- c(
+        "Cases" = "#ED553B",
+        "Tests"  = "#f2c82e"
+    )
+
+    p <- plot_ly(data, x = ~Date, y = ~Count, color = ~Type, text = ~Text,
+                 type = "bar", colors = colors, hoverinfo = "text"
+    ) %>%
     layout(barmode = "stack", xaxis = xaxis, yaxis = yaxis, title =
-             list(text = cap, xanchor = "left", x = 0), legend =
-             list(orientation = "h", font = list(size = 16))
+           list(text = cap, xanchor = "left", x = 0), legend =
+           list(orientation = "h", font = list(size = 16))
     ) %>%
     plotly::config(toImageButtonOptions = list(width = NULL, height = NULL))
-  
-  vroom_write(data, path = paste0(data_repo, today, "/plot8.csv"),
-              delim = ","
-  )
-  return(p)
-  
+
+    # vroom_write(data, path = paste0(data_repo, today, "/plot8.csv"),
+    #             delim = ","
+    # )
+    return(p)
 }
--- model/r_scripts/plots/plot_fig_9.R
+++ model/r_scripts/plots/plot_fig_9.R
@@ -1,41 +1,46 @@
+if ( Sys.getenv("production") == "TRUE" ) {
+  data_repo <- "~/cov-ind-19-data/"
+  today     <- Sys.getenv("today")
+} else {
+  data_repo <- "~/cov-ind-19-test/"
+  today     <- max(as.Date(grep("[0-9]", list.files(data_repo), value = T)))
+}
 
-plot_fig_9 = function(start.date = as.Date('2020-04-01')) {
-  
-  data <- vroom(paste0(data_repo, today, "/global_testing.csv"))
-  #today = '2020-04-17' # delete this later
-  data = vroom('C:/Users/mkleinsa/Box/Projects/covid-india/IRIS5/cov-ind-19-data/today/global_testing.csv') %>%
+plot_fig_9 <- function(start.date = as.Date('2020-04-01'))
+{
+    data <- vroom(paste0(data_repo, today, "/global_testing.csv")) %>%
     select(location, date, total_cases_per_million, total_tests_per_thousand) %>%
     group_by(location) %>%
     filter(!is.na(total_tests_per_thousand)) %>%
     filter(date == max(date)) %>%
-    drop_na() %>% 
+    drop_na() %>%
     mutate(Text = paste0('Location: ', location))
-  
+
   cap <- paste0("© COV-IND-19 Study Group. Last updated: ",
                 format(today, format = "%b %e"), sep = ' ')
-  
+
   title <- paste("Testing pattern across countries")
-  
+
   axis.title.font <- list(size = 16)
   tickfont        <- list(size = 16)
-  
+
   xaxis <- list(title = "Total cases per million", titlefont = axis.title.font, showticklabels = TRUE)
-  
+
   yaxis <- list(title = "Total tests per thousand", titlefont = axis.title.font,
                 tickfont = tickfont, zeroline = T)
   line_fmt = list(dash="solid", width = 1.5, color='black')
-  
+
   p <- plot_ly(data, x = ~total_cases_per_million, y = ~total_tests_per_thousand, color = ~location, text = ~Text,
                hoverinfo = "text"
   ) %>%
     layout(xaxis = xaxis, yaxis = yaxis, title = list(text = cap, xanchor = "left", x = 0)
     ) %>%
     plotly::config(toImageButtonOptions = list(width = NULL, height = NULL))
-  
+
   vroom_write(data, path = paste0(data_repo, today, "/plot9.csv"),
               delim = ","
   )
-  
+
   #m1 = lm(total_tests_per_thousand ~ total_cases_per_million, data = data)
   #p = add_lines(p, x = 0:max(data$total_cases_per_million), y = predict(m1), line = line_fmt, name = "Linear trend")
   p
