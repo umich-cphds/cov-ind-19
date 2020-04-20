b3d67828bfac0bbc441d262c198455c7fc7a73f0
Alexander Rix <alexanderjrix@gmail.com>
2020-04-20 10:50:41 -0400

    tweak fig 8 and reformat fig 9

--- model/r_scripts/plots/plot_fig_8.R
+++ model/r_scripts/plots/plot_fig_8.R
@@ -7,16 +7,19 @@ if (Sys.getenv("production") == "TRUE") {
     today     <- max(as.Date(grep("[0-9]", list.files(data_repo), value = T)))
 }
 
-plot_fig_8 <- function(start.date = as.Date('2020-04-01'))
+plot_fig_8 <- function(start.date = as.Date("2020-04-01"))
 {
     data <- vroom(paste0(data_repo, today, "/testing.csv")) %>%
     filter(Country == "India") %>%
     filter(Date >= start.date) %>%
     mutate(Date = as.factor(format(Date, format = "%b %e")),
-           Cases = c(NA, diff(Cases)), #format(Cases, big.mark = ",", scientific = F, trim = T),
-           Tests = c(NA, diff(Tests)), #format(Tests, big.mark = ",", scientific = F, trim = T),
-           Text_cases = paste0(Date, ": ", Cases, ' cases'),
-           Text_tests = paste0(Date, ": ", Tests, ' tests')) %>%
+           Cases = c(NA, diff(Cases)),
+           Tests = c(NA, diff(Tests)),
+           Text_cases = paste0(Date, ": ", format(Cases, big.mark = ",",
+                               scientific = F, trim = T), " cases"),
+           Text_tests = paste0(Date, ": ", format(Tests, big.mark = ",",
+                               scientific = F, trim = T), " tests")
+    ) %>%
     drop_na()
 
   data <- bind_rows(
@@ -25,15 +28,16 @@ plot_fig_8 <- function(start.date = as.Date('2020-04-01'))
       mutate(
           Count = Cases,
           Text = Text_cases,
-          Type = 'Cases'),
+          Type = "Cases"),
           data %>%
           select(-Cases, -Text_cases) %>%
-          mutate(Count = Tests, Text = Text_tests, Type = 'Tests')
+          mutate(Count = Tests, Text = Text_tests, Type = "Tests")
     ) %>%
+    mutate(Type = factor(Type, levels = c("Tests", "Cases"))) %>%
     select(Date, Count, Text, Type, Country)
 
     cap <- paste0("© COV-IND-19 Study Group. Last updated: ",
-                  format(today, format = "%b %e"), sep = ' ')
+                  format(today, format = "%b %e"))
 
     title <- paste("Daily confirmed positive cases and total tests in India")
 
@@ -47,7 +51,7 @@ plot_fig_8 <- function(start.date = as.Date('2020-04-01'))
                   tickfont = tickfont, zeroline = T)
     colors <- c(
         "Cases" = "#ED553B",
-        "Tests"  = "#f2c82e"
+        "Tests" = "#f2c82e"
     )
 
     p <- plot_ly(data, x = ~Date, y = ~Count, color = ~Type, text = ~Text,
@@ -62,5 +66,5 @@ plot_fig_8 <- function(start.date = as.Date('2020-04-01'))
     # vroom_write(data, path = paste0(data_repo, today, "/plot8.csv"),
     #             delim = ","
     # )
-    return(p)
+    p
 }
