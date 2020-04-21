4b86c8202841cb8e0f0f259989ff869229e3ae6d
Kleinsasser <mkleinsa@UMICH.EDU>
2020-04-21 13:40:01 -0400

    trend line and text for fig 9

--- model/r_scripts/plots/plot_fig_9.R
+++ model/r_scripts/plots/plot_fig_9.R
@@ -14,8 +14,11 @@ plot_fig_9 <- function(start.date = as.Date("2020-04-01"))
     filter(!is.na(total_tests_per_thousand)) %>%
     filter(date == max(date)) %>%
     drop_na() %>%
-    mutate(Text = paste0("Location: ", location))
-
+    mutate(Text = paste0("Location: ", location, '. ', '(x,y) = ', '(', total_cases_per_million, ', ', total_tests_per_thousand, ')'))
+    
+    lmpred = predict(lm(total_tests_per_thousand~total_cases_per_million, data = data),
+                        newdata = data.frame(total_cases_per_million = data$total_cases_per_million))
+    data$lmpred = lmpred  #= data.frame(lmfit = lmfit, x = data$total_cases_per_million)
     cap <- paste0("© COV-IND-19 Study Group. Last updated: ",
                 format(today, format = "%b %e"))
 
@@ -30,19 +33,21 @@ plot_fig_9 <- function(start.date = as.Date("2020-04-01"))
     yaxis <- list(title = "Total tests per thousand", titlefont = axis.title.font,
                   tickfont = tickfont, zeroline = T)
     line_fmt <- list(dash = "solid", width = 1.5, color = "black")
-
-    p <- plot_ly(data, x = ~total_cases_per_million, y = ~total_tests_per_thousand,
-               color = ~location, text = ~Text, hoverinfo = "text"
-    ) %>%
-        layout(xaxis = xaxis, yaxis = yaxis, title = list(text = cap, xanchor = "left", x = 0)
-    ) %>%
-    plotly::config(toImageButtonOptions = list(width = NULL, height = NULL))
-
-    # vroom_write(data, path = paste0(data_repo, today, "/plot9.csv"),
-    #           delim = ","
-    # )
-
-    #m1 = lm(total_tests_per_thousand ~ total_cases_per_million, data = data)
-    #p = add_lines(p, x = 0:max(data$total_cases_per_million), y = predict(m1), line = line_fmt, name = "Linear trend")
+   
+    p = plot_ly(data, type = "scatter", color = ~location, x = ~total_cases_per_million, y = ~total_tests_per_thousand,
+                mode = "line", hoverinfo = "text", hoverlabel = list(align = "left"), text = ~Text
+        ) %>%
+        add_fun(
+        function(p) {
+          p %>% 
+            add_segments(data = (data %>% arrange(desc(lmpred)))[c(1,nrow(data)),],
+                         x = ~total_cases_per_million[1], xend = ~total_cases_per_million[2], 
+                         y = ~lmpred[1],  yend = ~lmpred[2], 
+                         type = "scatter",
+                         showlegend = F, line = list(width = 1, color = 'gray'))
+        }
+      ) %>% layout(xaxis = xaxis, yaxis = yaxis, title = list(text = cap, xanchor = "left", x = 0)) %>% 
+      plotly::config(toImageButtonOptions = list(width = NULL, height = NULL))
+      
   p
 }
