a5dc8a3aeefbf21c3a6aedba332c1070b3e80098
Kleinsasser <mkleinsa@UMICH.EDU>
2020-04-24 14:34:36 -0400

    percent of country tested in hover text

--- model/r_scripts/plots/plot_fig_11.R
+++ model/r_scripts/plots/plot_fig_11.R
@@ -26,9 +26,93 @@ plot_fig_11 <- function(start.date = as.Date("2020-04-01"))
     group_by(location) %>%
     filter(!is.na(total_cases) & !is.na(total_tests)) %>%
     drop_na() %>%
-    filter(date > as.Date('2020-04-01')) %>%
+    filter(total_cases >= 100) %>%
     mutate(Percent = (total_cases/total_tests)*100)
-
+  
+  popsize = 
+    matrix(data = c('Argentina',44494502,
+                    'Australia',24982688,
+                    'Austria',8840521,
+                    'Belgium',11433256,
+                    'Bangladesh',161356039,
+                    'Bulgaria',7025037,
+                    'Bahrain',1569439,
+                    'Bolivia',11353142,
+                    'Canada',37057765,
+                    'Switzerland', 8513227,
+                    'Chile', 18729160,
+                    'Colombia', 49648685,
+                    'Costa Rica', 4999441,
+                    'Cuba', 11338138,
+                    'Czech Republic', 10629928,
+                    'Germany', 82905782,
+                    'Denmark', 5793636,
+                    'Ecuador', 17084357,
+                    'Spain', 46796540,
+                    'Estonia', 1321977,
+                    'Ethiopia', 109224559,
+                    'Finland', 5515525,
+                    'France', 66977107,
+                    'United Kingdom', 66460344,
+                    'Ghana', 29767108,
+                    'Greece', 10731726,
+                    'Croatia', 4087843,
+                    'Hungary', 9775564,
+                    'Indonesia', 267663435,
+                    'India', 1352617328,
+                    'Ireland', 4867309,
+                    'Iran', 81800269,
+                    'Iceland', 352721,
+                    'Israel', 8882800,
+                    'Italy', 60421760,
+                    'Japan', 126529100,
+                    'Kazakhstan', 18272430,
+                    'Kenya', 51393010,
+                    'South Korea', 51606633,
+                    'Lithuania', 2801543,
+                    'Luxembourg', 607950,
+                    'Latvia', 1927174,
+                    'Morocco', 36029138,
+                    'Mexico', 126190788,
+                    'Myanmar', 53708395,
+                    'Malaysia', 31528585,
+                    'Nigeria', 195874740,
+                    'Netherlands', 17231624,
+                    'Norway', 5311916,
+                    'New Zealand', 4841000,
+                    'Pakistan', 212215030,
+                    'Panama', 4176873,
+                    'Peru', 31989256,
+                    'Philippines', 106651922,
+                    'Poland', 37974750,
+                    'Portugal', 10283822,
+                    'Paraguay', 6956071,
+                    'Romania', 19466145,
+                    'Russia', 144478050,
+                    'Rwanda', 12301939,
+                    'Senegal', 15854360,
+                    'Singapore', 5638676,
+                    'El Salvador', 6420744,
+                    'Serbia', 6982604,
+                    'Slovakia', 5446771,
+                    'Slovenia', 2073894,
+                    'Sweden', 10175214,
+                    'Thailand', 69428524,
+                    'Tunisia', 11565204,
+                    'Turkey', 82319724,
+                    'Taiwan', 23780000,
+                    'Uruguay', 3449299,
+                    'United States', 326687501,
+                    'Vietnam', 95540395,
+                    'South Africa', 57779622), 
+           ncol = 2, byrow = TRUE) 
+  colnames(popsize) <- c('Country','Population')
+  popsize = as.data.frame(popsize)
+  popsize$Population = as.numeric(as.character(popsize$Population))
+  
+  data = left_join(data, popsize, by = c('location' = 'Country')) %>%
+    mutate(Percent_of_pop = (total_tests/Population)*100)
+  
   data <- bind_rows(
     data %>%
       select(-total_cases) %>%
@@ -47,12 +131,9 @@ plot_fig_11 <- function(start.date = as.Date("2020-04-01"))
   ) %>%
     mutate(Type = factor(Type, levels = c("Positive tests", "Tests"))) %>%
     select(-total_cases, - total_tests) %>%
-    mutate(Text = paste0(Text, "<br>Percent positive tests: ", format(Percent, digits = 3)))
-
-  # colors <- c(
-  #   "Tests" = "#b3b3b3",
-  #   "Cases" = "#138808"
-  # )
+    mutate(Text = paste0(Text, "<br>Percent positive tests: ", format(Percent, digits = 3), 
+                               "<br>Percent of population tested: ", format(Percent_of_pop, digits = 3)))
+  
 
   colors <- c(
     "Tests" = "#B7F1A0",
