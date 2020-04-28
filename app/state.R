347aecec33d8444efceb261498b31f6255fb6f61
Max Salvatore <38917700+maxsal@users.noreply.github.com>
2020-04-28 17:13:55 -0400

    remove quick/slow adherence

--- app/state.R
+++ app/state.R
@@ -31,20 +31,14 @@ generate_state_tab <- function(state, code)
           tags$li("Lockdown until May 3 with a gradual, moderate resumption of daily activities")
         ),
         "Because we are using SIR models to generate the forecast, we
-        explicitly delay changes in the basic reproduction number one week
-        (Figure 4a) and two weeks (Figure 4b) to capture delayed onset of
-        cases due to incubation period and timeliness of adherence.", "In
-        general terms, the one-week delay models (Figures 4a, 5a, and 5b) can
-        be thought of as 'quick adherence' to the lockdown measures, while
-        the 'two-week' delay models (Figures 4b, 6a, and 6b) can be though of
-        as 'slow adherence' to the lockdown measures.",
+        explicitly delay changes in the basic reproduction number one week to capture delayed onset of
+        cases due to incubation period.",
         HTML(paste0("All models assume a basic reproduction number of 2 under
           no intervention. The implied R", tags$sub("0"), " is 1.5 under Scenario 2.")),
         HTML(paste0("We further assume the R", tags$sub("0"), " drops to 0.8 under lockdown and then gradually rises back up to 1.5 after the lockdown ends over a three week period ('lockdown with moderate return').")),
           "You can hover of the bars for dates and counts. Also, please note the dotted line represents the upper confidence interval for the lockdown scenario (3), which is closest to the current intervention.",
           "Our codes are available on ", a("GitHub", .noWS = "outside", href = "https://github.com/umich-cphds/cov-ind-19") ," and so users can change the nature of interventions."),
-        h2("Quick adherence (one-week delay)"),
-        h3("Figure 4a (please note that the y-axis is in log base-10 scale; hover over the bars for count estimates and upper credible limits)"),
+        h3("Figure 4 (please note that the y-axis is in log base-10 scale; hover over the bars for count estimates and upper credible limits)"),
         plotlyOutput(paste0(code, "_p4"), height = "600px"),
      ),
      column(width = 1)
