fa3692babb57c4e16cc45a3da3a56069b75e316e
Kleinsasser <mkleinsa@UMICH.EDU>
2020-05-14 18:32:09 -0400

    green everywhere

--- model/r_scripts/plots/plot_fig_12.R
+++ model/r_scripts/plots/plot_fig_12.R
@@ -68,12 +68,12 @@ plot_fig_12 <- function(start.date = "2020-06-01") {
     fac_inc_data %>%
     mutate(Dates = as.Date(Dates)) %>%
     filter(Dates < as.Date('2020-09-16') & Dates >= as.Date(start.date),
-           color == 'Cautious return' | color == 'Moderate return') %>%
+           color == 'No Intervention') %>%
     left_join(statenames, by = c('State' = 'abbrev'))
   
   top20inc = 
     fac_inc_data %>% 
-    filter(color == 'Moderate return') %>%
+    filter(color == 'No Intervention') %>%
     group_by(full) %>% 
     summarise(maxproj = max(value)) %>%
     arrange(desc(maxproj)) %>%
@@ -89,12 +89,12 @@ plot_fig_12 <- function(start.date = "2020-06-01") {
     fac_cumul_data %>%
     mutate(Dates = as.Date(Dates)) %>%
     filter(Dates < as.Date('2020-09-16') & Dates >= as.Date(start.date),
-           color == 'Cautious return' | color == 'Moderate return') %>%
+           color == 'No Intervention') %>%
     left_join(statenames, by = c('State' = 'abbrev'))
   
   top20cumul = 
     fac_cumul_data %>% 
-    filter(color == 'Moderate return') %>%
+    filter(color == 'No Intervention') %>%
     group_by(full) %>% 
     summarise(maxproj = max(value)) %>%
     arrange(desc(maxproj)) %>%
@@ -114,9 +114,12 @@ plot_fig_12 <- function(start.date = "2020-06-01") {
 
   fmt1 <- function(x) paste(round_any(10 ^ x / 1000, 0.01) , "K", sep = "")
   
-  p12a <- ggplot(data = fac_inc_data %>% filter(color == 'Moderate return'), aes(x = Dates, y = value, group = full)) +
-    facet_wrap(~full, ncol = 4) + geom_line(aes(color = 'Moderate return'), size = 1.2) +
-    geom_line(data = fac_inc_data %>% filter(color == 'Cautious return'), aes(color = 'Cautious return'), size = 1) +
+  p12a <- ggplot(data = fac_inc_data, aes(x = Dates, y = value, group = full)) +
+    facet_wrap(~full, ncol = 4) + geom_line(color = '#36A30B', size = 1.8) +
+    geom_point(shape = 3, size = 0.2) + 
+    geom_point(data = fac_inc_data %>% group_by(full) %>% mutate(Datemax = max(Dates)) %>% filter(Dates == Datemax) %>% distinct(Datemax, .keep_all = TRUE),
+               aes(x = Datemax, y = value, group = State), size = 2) +
+    #geom_line(data = fac_inc_data %>% filter(color == 'Cautious return'), aes(color = 'Cautious return'), size = 1) +
     theme_bw() +
     xlab('\nDate') + ylab('Number of projected daily cases\n') +
     theme(strip.text.x = element_text(size=12, face="bold", hjust = 0, color = 'black'),
@@ -129,7 +132,7 @@ plot_fig_12 <- function(start.date = "2020-06-01") {
           axis.text.y = element_text(size = 13),
           legend.position = 'bottom',
           legend.text = element_text(size=12)) +
-    scale_x_date(sec.axis = ~ .) +
+    #scale_x_date(sec.axis = ~ .) +
     scale_y_continuous(sec.axis = sec_axis(~ ., labels = NotFancy), labels = NotFancy) +
     # scale_y_log10(breaks = scales::trans_breaks("log10", function(x) 10^x),
     #               labels = scales::trans_format("log10", function(x) paste0(format(10 ^ x /1000, sci = F), "K")),
@@ -139,14 +142,16 @@ plot_fig_12 <- function(start.date = "2020-06-01") {
     # )) +
     labs(title = '',
          subtitle = subtitle,
-         caption = caption) +
-    scale_color_manual(values = c('Moderate return' = "#0472CF", 'Cautious return' = "#173F5F"),
-                       name = 'Return strategy')
+         caption = caption)# +
+    #scale_color_manual(values = c('Moderate return' = "#0472CF", 'Cautious return' = "#173F5F"),
+    #                   name = 'Return strategy')
 
-
-  p12b <- ggplot(data = fac_cumul_data %>% filter(color == 'Moderate return'), aes(x = Dates, y = value, group = full)) +
-    facet_wrap(~full, ncol = 4) + geom_line(aes(color = "Moderate return"), size = 1.2) +
-    geom_line(data = fac_cumul_data %>% filter(color == 'Cautious return'), aes(color = "Cautious return"), size = 1) +
+  p12b <- ggplot(data = fac_cumul_data, aes(x = Dates, y = value, group = full)) +
+    facet_wrap(~full, ncol = 4) + geom_line(color = '#36A30B', size = 1.8) +
+    geom_point(shape = 3, size = 0.2) + 
+    geom_point(data = fac_cumul_data %>% group_by(full) %>% mutate(Datemax = max(Dates)) %>% filter(Dates == Datemax) %>% distinct(Datemax, .keep_all = TRUE),
+               aes(x = Datemax, y = value, group = State), size = 2) +
+    #geom_line(data = fac_inc_data %>% filter(color == 'Cautious return'), aes(color = 'Cautious return'), size = 1) +
     theme_bw() +
     xlab('\nDate') + ylab('Cumulative number of projected daily cases\n') +
     theme(strip.text.x = element_text(size=12, face="bold", hjust = 0, color = 'black'),
@@ -159,7 +164,7 @@ plot_fig_12 <- function(start.date = "2020-06-01") {
           axis.text.y = element_text(size = 13),
           legend.position = 'bottom',
           legend.text = element_text(size=12)) +
-    scale_x_date(sec.axis = dup_axis()) +
+    #scale_x_date(sec.axis = ~ .) +
     scale_y_continuous(sec.axis = sec_axis(~ ., labels = NotFancy), labels = NotFancy) +
     # scale_y_log10(breaks = scales::trans_breaks("log10", function(x) 10^x),
     #               labels = scales::trans_format("log10", function(x) paste0(format(10 ^ x /1000, sci = F), "K")),
@@ -169,9 +174,36 @@ plot_fig_12 <- function(start.date = "2020-06-01") {
     # )) +
     labs(title = '',
          subtitle = subtitle,
-         caption = caption) +
-    scale_color_manual(values = c('Moderate return' = "#0472CF", 'Cautious return' = "#173F5F"),
-                       name = 'Return strategy')
+         caption = caption)# +
+
+  # p12b <- ggplot(data = fac_cumul_data %>% filter(color == 'Moderate return'), aes(x = Dates, y = value, group = full)) +
+  #   facet_wrap(~full, ncol = 4) + geom_line(aes(color = "Moderate return"), size = 1.2) +
+  #   geom_line(data = fac_cumul_data %>% filter(color == 'Cautious return'), aes(color = "Cautious return"), size = 1) +
+  #   theme_bw() +
+  #   xlab('\nDate') + ylab('Cumulative number of projected daily cases\n') +
+  #   theme(strip.text.x = element_text(size=12, face="bold", hjust = 0, color = 'black'),
+  #         strip.background = element_blank(),
+  #         panel.grid.minor.x = element_blank(),
+  #         panel.border = element_blank(),
+  #         axis.title.x = element_text(size = 14), #, face = "bold"
+  #         axis.title.y = element_text(size = 14), #, face = "bold"
+  #         axis.text.x = element_text(angle = 20, size = 14),
+  #         axis.text.y = element_text(size = 13),
+  #         legend.position = 'bottom',
+  #         legend.text = element_text(size=12)) +
+  #   scale_x_date(sec.axis = dup_axis()) +
+  #   scale_y_continuous(sec.axis = sec_axis(~ ., labels = NotFancy), labels = NotFancy) +
+  #   # scale_y_log10(breaks = scales::trans_breaks("log10", function(x) 10^x),
+  #   #               labels = scales::trans_format("log10", function(x) paste0(format(10 ^ x /1000, sci = F), "K")),
+  #   #               sec.axis = sec_axis(~ .,
+  #   #                   breaks = scales::trans_breaks("log10", function(x) 10^x),
+  #   #                   labels = scales::trans_format("log10", function(x) paste0(format(10 ^ x /1000, sci = F), "K"))
+  #   # )) +
+  #   labs(title = '',
+  #        subtitle = subtitle,
+  #        caption = caption) +
+  #   scale_color_manual(values = c('Moderate return' = "#0472CF", 'Cautious return' = "#173F5F"),
+  #                      name = 'Return strategy')
 
   list(p12a = p12a, p12b = p12b)
 
