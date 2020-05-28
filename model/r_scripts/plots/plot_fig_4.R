2e6e4c4ab1b3ce6427dfeb6b21c18e07841ab7ce
Max Salvatore <38917700+maxsal@users.noreply.github.com>
2020-05-28 16:17:47 -0400

    Update plot_fig_4.R

--- model/r_scripts/plots/plot_fig_4.R
+++ model/r_scripts/plots/plot_fig_4.R
@@ -1,52 +1,48 @@
 library(tidyverse)
-library(vroom)
 library(plotly)
+
 plot_fig_4 <- function(forecast, start.date = as.Date("2020-03-01"),
                         end.date = as.Date("2020-05-31"))
 {
-    data <- vroom(paste0(data_repo, today, "/1wk/", forecast,
-                            "_figure_4_data.csv")
+    data <- read_tsv(paste0(data_repo, today, "/1wk/", forecast,
+                            "_plot_data.txt"), col_types = cols()
     ) %>%
     mutate(
-        color = factor(color, levels= c("Observed", "No intervention",
-            "Social distancing", "Lockdown with moderate release")),
-        date.fmt = format(Dates, format("%b %e")),
-        val.fmt = format(value, big.mark = ",", scientific = F, trim = T),
-        ci.fmt = format(upper_ci, big.mark = ",", scientific = F, trim = T)
+        scenario = as.factor(scenario),
+        date_fmt = format(date, format("%b %e")),
+        val_fmt  = format(value, big.mark = ",", scientific = F, trim = T),
+        ci_fmt   = format(upper_ci, big.mark = ",", scientific = F, trim = T)
     ) %>%
     mutate(
-        color = recode(color, "Lockdown with moderate release" = "Lockdown with moderate return"),
-        j = color == "Lockdown with moderate return"
+        j = scenario == "Moderate return"
     ) %>%
     #filter(Dates <= end.date) %>%
     mutate(text = case_when(
-        color == "Observed" ~ paste0(date.fmt, ": ", val.fmt, " observed cases"),
-        TRUE ~ paste0(paste0(date.fmt, ": ", val.fmt, " projected cases"),
-                      paste0("<br>Projection upper CI: ", ci.fmt, " cases<br>")
+        scenario == "Observed" ~ paste0(date_fmt, ": ", val_fmt, " observed cases"),
+        scenario != "Observed" ~ paste0(paste0(date_fmt, ": ", val_fmt, " projected cases"),
+                      paste0("<br>Projection upper CI: ", ci_fmt, " cases<br>")
         )
     )) %>%
-    filter(Dates <= Sys.Date() + 30 & Dates >= Sys.Date() - 14)
-
-    title <- paste("Cumulative number of COVID-19 cases in India compared",
-                   "to other countries affected by the pandemic")
+    filter(date <= Sys.Date() + 30 & date >= Sys.Date() - 14) %>%
+    filter(scenario %in% c("Observed", "Moderate return", "Cautious return"))
 
     cap <- paste0("\uA9 COV-IND-19 Study Group. Last updated: ",
                   format(today, format = "%b %e"), sep = ' ')
 
-    axis.title.font <- list(size = 16)
+    axis_title_font <- list(size = 16)
     tickfont        <- list(size = 16)
 
-    xaxis <- list(title = "", titlefont = axis.title.font, showticklabels = TRUE,
+    xaxis <- list(title = "", titlefont = axis_title_font, showticklabels = TRUE,
                   tickangle = -30, showline = T, zeroline = T, x0 =
                   format(today, format = "%b %e"))
 
     yaxis <- list(title = "Cumulative number of cases", type = "log",
-                  dtick = 1, titlefont = axis.title.font, zeroline = T,
+                  dtick = 1, titlefont = axis_title_font, zeroline = T,
                   showline = T)
 
-    colors <- c("#979799", "#ED553B", "#f2c82e", "#173F5F")
-    p <- plot_ly(data, x = ~ Dates, y = ~ value, text = ~text,
-                 color = ~color, colors = colors, name = ~color, type = "bar",
+    colors <- c("#979799", "#f2c82e", "#173F5F")
+    p <- plot_ly(data, x = ~ date, y = ~ value, text = ~text,
+                 color = ~scenario, colors = colors, name = ~scenario, type = "bar",
                  hoverinfo = "text", hoverlabel = list(align = "left")
     ) %>%
     layout(barmode = "overlay", xaxis = xaxis, yaxis = yaxis,
@@ -57,8 +53,8 @@ plot_fig_4 <- function(forecast, start.date = as.Date("2020-03-01"),
                          line = list(color = "#eee", size = 3)
                      )
     ) %>%
-    add_trace(data = filter(data, j), x = ~Dates, y = ~upper_ci,
-              name = paste(filter(data, j)$color, "upper CI"), type = "scatter",
+    add_trace(data = filter(data, j), x = ~date, y = ~upper_ci,
+              name = paste(filter(data, j)$scenario, "upper CI"), type = "scatter",
               mode = "lines", line = list(width = 3, dash = "dash")
     ) %>%
     plotly::config(toImageButtonOptions = list(width = NULL, height = NULL))
