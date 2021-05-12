7f09fed6c757085b96099c1b2df0b33f4f7b6ee0
Daniel Barker <danbarke@umich.edu>
2021-05-12 08:43:24 -0400

    Revert "Run all states, not just top 20"
    This reverts commit 08d4d090ce82d5aaaa5c5b5edb7496bcd0064ba2.
    
    Conflicts:
    	model/r_scripts/get_states.R

--- model/r_scripts/get_states.R
+++ model/r_scripts/get_states.R
@@ -15,6 +15,7 @@ today <- Sys.getenv("today")
 x <- vroom(paste0(data_repo, today, "/covid19india_data.csv"), col_types = cols()) %>%
 group_by(State) %>%
 filter(Date == max(Date) & State != "un" & State != "la" & State != "dd") %>%
-ungroup()
+ungroup() %>%
+top_n(20, Cases)
 
 cat(x$State, "\n")
