dcb8efd19eecc97b862d390e44e1765aefc38d6b
Daniel Barker <danbarke@umich.edu>
2021-05-18 16:55:14 -0400

    Add rest of states for SEIR

--- model/r_scripts/get_rest_of_states.R
+++ model/r_scripts/get_rest_of_states.R
@@ -8,13 +8,13 @@ today <- Sys.getenv("today")
 
 x_20 <- vroom(paste0(data_repo, "/", today, "/covid19india_data.csv"), col_types = cols()) %>%
 group_by(State) %>%
-filter(Date == max(Date) & State != "un" & State != "la" & State != "dd") %>%
+filter(Date == max(Date) & State != "un" & State != "la" & State != "dd" & State != "hp" & State != "py") %>%
 ungroup() %>%
 top_n(20, Cases)
 
 x_all <- vroom(paste0(data_repo, "/", today, "/covid19india_data.csv"), col_types = cols()) %>%
 group_by(State) %>%
-filter(Date == max(Date) & State != "un" & State != "la" & State != "dd") %>%
+filter(Date == max(Date) & State != "un" & State != "la" & State != "dd" & State != "hp" & State != "py") %>%
 ungroup()
 
 x = setdiff(x_all, x_20)
