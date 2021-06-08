6de920e6881515e3ebfff2fdb97441e5ae6b5ee9
Michael Kleinsasser <mkleinsa@umich.edu>
2021-06-08 12:04:53 -0400

    20 in wrong place

--- model/r_scripts/generate_plots.R
+++ model/r_scripts/generate_plots.R
@@ -96,7 +96,7 @@ for (state in states.to.forecast) {
 source(paste0(code_repo, "/app/sum_table_app.R"))
 data$gt <- India_gt_table()
 gtsave(data$gt$point_in_time, filename = path.expand(paste0(data_repo, "/", today, "/COVIND_table_point_in_time20.png")))
-gtsave(data$gt$cumulative, filename = path.expand(paste0(data_repo, "/", today, "/COVIND_table_cumulative.png20")))
+gtsave(data$gt$cumulative, filename = path.expand(paste0(data_repo, "/", today, "/COVIND_table_cumulative20.png")))
 data$gt = data$gt$full20
 
 save(data, file = paste0(data_repo, "/", today, "/data.RData"))
