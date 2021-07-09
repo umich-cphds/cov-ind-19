9b5243441693c2e7cd7d992ee5e142b5d7bbb6b2
Max Salvatore <38917700+maxsal@users.noreply.github.com>
2021-07-09 15:00:12 -0400

    Update state_gen_seirfansy.R

--- model/r_scripts/state_gen_seirfansy.R
+++ model/r_scripts/state_gen_seirfansy.R
@@ -1,12 +1,8 @@
-data_repo <- Sys.getenv("data_repo")
-code_repo <- Sys.getenv("code_repo")
-today <- as.Date(Sys.getenv("today"))
-
-setwd(paste0(code_repo, "/model/r_scripts/"))
+library(here)
 source("libraries.R")
 
-f <- list.files(paste0(code_repo, "/model/r_scripts/functions"))
-sapply(paste0("functions/", f), source)
+f <- list.files(here("functions"))
+for (i in seq_along(f)) { source(here("functions", f[i])) }
 
 # Set variables based on testing or production
 if ( Sys.getenv("production") == "TRUE" ) {
@@ -19,10 +15,12 @@ if ( Sys.getenv("production") == "TRUE" ) {
 	opt_num   <- 1   #default 200
 }
 
+data_repo <- Sys.getenv("data_repo")
+today <- as.Date(Sys.getenv("today"))
 # specs -----------
 state    <- Sys.getenv("state")
 max_date <- as.Date(today - 1)
-min_date <- as.Date("2020-04-01")
+min_date <- as.Date(max_date - 99)
 obs_days <- length(as.Date(min_date):as.Date(max_date))
 t_pred   <- 150 # number of predicted days
 N        <- get_pop(state)
@@ -49,7 +47,7 @@ data <- data %>% dplyr::filter(date >= min_date)
 
 mCFR <- tail(cumsum(data$Deceased) / cumsum(data$Deceased + data$Recovered), 1)
 
-phases <- get_phase(start_date = "2020-04-01")
+phases <- get_phase(start_date = min_date, end_date = max_date)
 
 # predict -----------
 result    <- SEIRfansy.predict(
@@ -73,7 +71,7 @@ result    <- SEIRfansy.predict(
 )
 
 # directory ----------
-wd <- paste0(data_repo, "/", today, "/seirfansy")
+wd <- paste0(data_repo, today)
 if (!dir.exists(wd)) {
   dir.create(wd, recursive = TRUE)
   message("Creating ", wd)
@@ -84,8 +82,8 @@ pred_clean <- clean_prediction(result$prediction,
                                obs_days = obs_days,
                                t_pred   = t_pred)
 
-write_tsv(pred_clean, paste0(wd, "/prediction_", state, ".txt"))
-write_tsv(as_tibble(result$mcmc_pars, .name_repair = "unique"), paste0(wd, "/prediction_pars_", state, ".txt"))
+write_tsv(pred_clean, paste0(data_repo, today, "/prediction_", state, ".txt"))
+write_tsv(result$mcmc_pars, paste0(data_repo, today, "/prediction_pars_", state, ".txt"))
 
 p_pred <- pred_clean %>%
   filter(section == "positive_reported") %>%
@@ -120,4 +118,4 @@ impo <- tibble(
   "ifr"                   = ifr[obs_days + 1]
 )
 
-write_tsv(impo, paste0(wd, "/important_", state, ".txt"))
+write_tsv(impo, paste0(data_repo, today, "/important_", state, ".txt"))
