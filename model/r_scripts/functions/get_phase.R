93cc89cec1cbd96357dcadd0da3e28138c150d71
Max Salvatore <38917700+maxsal@users.noreply.github.com>
2021-07-15 11:13:43 -0400

    Update get_phase.R

--- model/r_scripts/functions/get_phase.R
+++ model/r_scripts/functions/get_phase.R
@@ -1,14 +1,15 @@
-get_phase <- function(start_date = min_date, end_date = max_date) {
+get_phase <- function(start_date = min_date, end_date = max_date, phase_length = 15) {
   
   tmp_dates <- seq.Date(from = as.Date(start_date),
                         to   = as.Date(end_date),
                         by    = "day")
   
-  tmp_phases <- c(1, 
-    tmp_dates %>% lubridate::day() %>% which(x = . == 1)) %>%
+  tmp_phases <- c(1,
+    tmp_dates %>% which(x = . %in% rev(rev(tmp_dates)[seq(1, length(tmp_dates), phase_length)]))) %>%
     unique()
+  
+  tmp_phases <- tmp_phases[!tmp_phases %in% 2:phase_length]
   
   return(tmp_phases)
   
 }
-
