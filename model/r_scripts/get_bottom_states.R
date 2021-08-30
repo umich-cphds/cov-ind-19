6f502334780a1adcc9008050dc59c4dca791ef1a
Daniel Barker <danbarke@umich.edu>
2021-08-30 08:39:40 -0400

    Filter out ld as it is failing to run to completion

--- model/r_scripts/get_bottom_states.R
+++ model/r_scripts/get_bottom_states.R
@@ -8,7 +8,7 @@ today <- Sys.getenv("today")
 
 x <- vroom(paste0(data_repo, "/", today, "/covid19india_data.csv"), col_types = cols()) %>%
   group_by(State) %>%
-  filter(Date == max(Date) & State != "un" & State != "la" & State != "dd" & State != "hp" & State != "py" & State != "dn" & State != "ga" ) %>%
+  filter(Date == max(Date) & State != "un" & State != "la" & State != "dd" & State != "hp" & State != "py" & State != "dn" & State != "ga" & State != "ld" ) %>%
   ungroup() %>%
   top_n(-13, Cases)
 
