a95cf4ff2618ed14425ab6188ecaa2bd9c05d55b
Michael Kleinsasser <mkleinsa@umich.edu>
2021-08-31 11:18:58 -0400

    new data source for vax data

--- model/r_scripts/plots/plot_fig_vax.R
+++ model/r_scripts/plots/plot_fig_vax.R
@@ -5,58 +5,20 @@ library(tidyverse)
 library(ggtext)
 library(scales)
 library(tidyr)
+library(covid19india)
 })
 
 plot_fig_vax = function() {
-  # vax_dat <- suppressMessages(vroom("http://api.covid19india.org/csv/latest/vaccine_doses_statewise.csv")) %>%
-  #   pivot_longer(
-  #     names_to = "date",
-  #     values_to = "vaccines",
-  #     -State
-  #   ) %>%
-  #   mutate(
-  #     date = as.Date(date, format = "%d/%m/%Y")
-  #   ) %>%
-  #   dplyr::rename(
-  #     state = State
-  #   ) %>%
-  #   group_by(state) %>%
-  #   arrange(date) %>%
-  #   mutate(
-  #     daily_vaccines = vaccines - dplyr::lag(vaccines)
-  #   ) %>%
-  #   ungroup()
   
-  vax_dat <- read_csv("http://api.covid19india.org/csv/latest/vaccine_doses_statewise.csv") %>%
-    pivot_longer(names_to = "date", values_to = "count", -State) %>%
-    mutate(date = as.Date(date, "%e/%m/%Y")) %>%
-    arrange(date) %>%
-    filter(State == "Total") %>%
-    mutate(count = replace(count, count == 0, NA),
-           lag = count - dplyr::lag(count)
-    ) %>% fill()
+  vax_dat = get_state_vax() %>% 
+    filter(place == "India") %>% 
+    mutate(date = as.Date(date, "%e/%m/%Y"))
   
-  # vax_dat %>%
-  #   ggplot(aes(x = date, y = vaccines)) +
-  #   geom_line() +
-  #   labs(
-  #     title = "Cumulative COVID-19 vaccines delivered in India",
-  #     x     = "Date",
-  #     y     = "Number of vaccines",
-  #     caption = "**Source:** covid19india.org<br>**\uA9 COVIND-19 Study Group**"
-  #   ) +
-  #   scale_y_continuous(labels = comma) +
-  #   theme_minimal()+
-  #   theme(
-  #     plot.title   = element_text(face = "bold", hjust = 0.5),
-  #     plot.caption = element_markdown(hjust = 0)
-  #   )
-  # lag = ifelse(lag < 0, 0, lag),
   vax_india = vax_dat %>% 
     rename(Day = date) %>% 
-    mutate(text = paste0("India", "<br>", Day, ": ", format(lag, big.mark = ","),
+    mutate(text = paste0("India", "<br>", Day, ": ", format(daily_doses, big.mark = ","),
                          " daily vaccines<br>")) %>% 
-    filter(Day >= as.Date("2021-02-15"))
+    filter(Day >= as.Date("2021-03-15"))
   
   india_color <- "#138808"
   names(india_color) <- "India"
@@ -72,7 +34,7 @@ plot_fig_vax = function() {
   vax.yaxis <- list(title = "Number of vaccines", titlefont =
                       axis.title.font, zeroline = F, showline = F)
   
-  case_plot <- plot_ly(vax_india, x = ~ Day, y = ~ lag, text = ~ text, color = I("#138808"),
+  case_plot <- plot_ly(vax_india, x = ~ Day, y = ~ daily_doses, text = ~ text, color = I("#138808"),
                        hoverinfo = "text", type = "bar", hoverlabel = list(align = "left"),
                        showlegend = F, line = list(width = 3)
   ) %>%
