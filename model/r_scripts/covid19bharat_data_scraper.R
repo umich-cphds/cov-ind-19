b85f6db1d8da1bbd4d3d8f9ca08f93306b36e0c5
Mike Kleinsasser <mkleinsa@umich.edu>
2021-12-07 13:27:09 -0500

    read, process, merge new data

--- model/r_scripts/covid19bharat_data_scraper.R
+++ model/r_scripts/covid19bharat_data_scraper.R
@@ -8,6 +8,8 @@ path_current = "https://data.covid19bharat.org/v4/min/data.min.json"
 raw_historical = content(GET(path_historical))
 raw_current = content(GET(path_current))
 
+today = Sys.Date()
+
 raw_historical = 
   path_historical %>% 
   GET() %>% 
@@ -20,9 +22,10 @@ raw_historical =
   filter(x6 == "total") %>% 
   select(-x6) %>%
   pivot_wider(names_from = "x7") %>% 
-  select(-other, -vaccinated1, -vaccinated2) %>% 
+  select(-other) %>% 
   mutate(place = tolower(x1)) %>% 
-  select(place, date, confirmed, recovered, deceased, tested)
+  select(place, date, confirmed, recovered, deceased, tested, vaccinated1, vaccinated2) %>% 
+  filter(date != today)
 
 raw_current = 
   path_current %>% 
@@ -31,9 +34,19 @@ raw_current =
   unlist() %>% 
   enframe() %>% 
   separate(name, into = paste0("x", 1:10)) %>% 
-  filter((x2 %in% c("total") & x3 %in% c("tested", "confirmed", "recovered", "deceased")) | (x2 == "meta" & x3 == "date")) %>% 
+  filter((x2 %in% c("total") & x3 %in% c("tested", "confirmed", "recovered", "deceased", "vaccinated1", "vaccinated2")) | (x2 == "meta" & x3 == "date")) %>% 
   select(x1, x3, value) %>% 
   mutate(place = tolower(x1)) %>% 
   select(-x1) %>% 
-  pivot_wider(names_from = "x3")
+  pivot_wider(names_from = "x3") %>% 
+  mutate(confirmed   = as.numeric(confirmed), 
+         recovered   = as.numeric(recovered), 
+         deceased    = as.numeric(deceased), 
+         tested      = as.numeric(tested), 
+         vaccinated1 = as.numeric(vaccinated1), 
+         vaccinated2 = as.numeric(vaccinated2))
+
+data = 
+  raw_historical %>% 
+  bind_rows(raw_current)
 
